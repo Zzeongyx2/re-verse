e9acf713997caf70f0d282e867ccf092e1a67818
yoonsunny17 <jyoonsun0217@gmail.com>
2022-10-20 23:50:24 +0900

    [FE] refactor: move neonlogo to atom component

---
+++ frontend/src/UI/atoms/NeonLogo.tsx
@@ -0,0 +1,40 @@
+import { Box, Container, Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import styles from "../../styles/Main.module.css";
+import "../../index.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Beon",
+  },
+});
+
+function NeonLogo() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        style={{
+          margin: "0",
+          backgroundColor: "#1A1F28",
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Container>
+          <Box className={styles.board}>
+            <Typography variant="h1" className={styles.logo}>
+              RE-VERSE
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default NeonLogo;
--- frontend/src/UI/pages/Main.tsx
+++ frontend/src/UI/pages/Main.tsx
@@ -1,41 +1,7 @@
-import React from "react";
-import { Box, Container, Typography } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import styles from "../../styles/Main.module.css";
-import "../../index.css";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: "Beon",
-  },
-});
+import NeonLogo from "../atoms/NeonLogo";
 
 function Main() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box
-        style={{
-          margin: "0",
-          backgroundColor: "#1A1F28",
-          height: "100vh",
-          width: "100vw",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Container>
-          <Box className={styles.board}>
-            <Typography variant="h1" className={styles.logo}>
-              RE-VERSE
-            </Typography>
-          </Box>
-        </Container>
-      </Box>
-    </ThemeProvider>
-  );
+  return <NeonLogo />;
 }
 
 export default Main;
