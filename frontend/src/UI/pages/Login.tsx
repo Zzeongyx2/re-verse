b6373eaec9ccd8ef85a18c32b6646ad280bc1082
지용현 <yung5487@gmail.com>
2022-10-20 23:09:07 +0900

    [FE] feat : create signin page

--- frontend/src/UI/pages/Login.tsx
+++ frontend/src/UI/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Grid, TextField } from '@mui/material';
 import {useState} from "react"
-
+import {BsInfoCircle} from "react-icons/bs"
 
 // interface LoginInput {
 //     id: string,
@@ -24,22 +24,42 @@ function Login() {
 
     const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key == 'Enter') {
-
+            clickLogin()
         }
     }
+
+    const clickLogin = () =>{
+        //axios 요청 보내기
+        console.log("33줄");
+        console.log({id: id, pw: pw});
+        
+        if (id.trim() == '') {
+            alert("아이디를 입력하세요");
+            return;
+        } else if (pw.trim() == '') {
+            alert("비밀번호를 입력하세요.")
+            return;
+        }
+
+        console.log("axios 로그인 요청 ");
+        
+    }
+
+
     // const loginInput: LoginInput = {
     //     id: "",
     //     pw: ""
     // }
     return (
         <Box sx={{width: 300, mx:10, my:5}}>
-            <Grid container spacing={2} justifyContent="center">
+            <Grid container spacing={3} justifyContent="center">
                 <Grid item xs={12}>
                     <TextField
                         value={id}
                         autoComplete="email"
                         fullWidth
                         label="아이디"
+                        size="small"
                         onChange={idHandleChange}
                         // error={true}
                         // helperText=""
@@ -51,12 +71,22 @@ function Login() {
                         fullWidth
                         label="비밀번호"
                         type="password"
+                        size="small"
                         onChange={pwHandleChange}
                         onKeyUp={handleKeyUp}
                 />
                 </Grid>
-                <Grid item xs={8} sx={{alignItems:"center"}}>
-                    <button>로그인</button>
+                <Grid item xs={8}>
+                    <Box sx={{
+                        alignItems: "cneter",
+                        justifyContent:"center",
+                        display: "flex",
+                        flexDirection: "column",
+                        textAlign:"center"
+                   }}>
+                        <button type="button" onClick={() => { clickLogin() }} >로그인</button>
+                        <a href="/signin" style={{ fontSize: 11, marginTop:10, textDecoration:"none", color: "black"}}><BsInfoCircle/> 아직 회원이 아니신가요?</a>
+                    </Box>
                 </Grid>
             </Grid>
 
