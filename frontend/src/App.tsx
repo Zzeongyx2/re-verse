a8f5b269a476904e58be3cf99e8038073ac36d1c
yoonsunny17 <jyoonsun0217@gmail.com>
2022-10-21 10:35:29 +0900

    [FE] fix: update router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './UI/pages/Login';
-import Signin from './UI/pages/Signin';
+import Login from "./UI/pages/Login";
+import Signin from "./UI/pages/Signin";
+import Main from "./UI/pages/Main";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signin" element={<Signin />} />
-        
       </Routes>
     </BrowserRouter>
   );
