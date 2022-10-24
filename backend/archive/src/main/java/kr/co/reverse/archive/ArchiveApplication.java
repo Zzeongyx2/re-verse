01ab33b5cd346917d1eaac381305460fb06242d9
kancho1216 <kancho1216@naver.com>
2022-10-24 17:20:22 +0900

    [BE] settings: archive server
    Former-commit-id: 5e4577232780ca6c54af44599bfa6201324e17b7

---
+++ backend/archive/src/main/java/kr/co/reverse/archive/ArchiveApplication.java
@@ -0,0 +1,13 @@
+package kr.co.reverse.archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchiveApplication.class, args);
+	}
+
+}
