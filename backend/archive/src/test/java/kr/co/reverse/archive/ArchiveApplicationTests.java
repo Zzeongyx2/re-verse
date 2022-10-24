2076c3ca5d76b9f9375eea04e5692a9a455329dd
kancho1216 <kancho1216@naver.com>
2022-10-24 17:20:22 +0900

    [BE] settings: archive server
    Former-commit-id: c43efea629df13ef17d4e7e863d803787f47fe0a

---
+++ backend/archive/src/test/java/kr/co/reverse/archive/ArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.reverse.archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
