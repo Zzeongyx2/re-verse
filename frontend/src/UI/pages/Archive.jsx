3abc9f5189ed7d86238701f7425b2685cdbbc3d2
yoonsun <jyoonsun0217@gmail.com>
2022-10-25 17:47:44 +0900

    [FE] feat: breadcrumbs  - still working
    Former-commit-id: 6e58f6738d09cdb763c7efb4f4bc523b391a9a82

--- frontend/src/UI/pages/Archive.jsx
+++ frontend/src/UI/pages/Archive.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@chakra-ui/react";
+import Navbar from "../organisms/Navbar";
 
 function Archive() {
   return (
-    <Box className="bg-base2 h-screen">
+    <Box className="bg-base2 h-screen text-white">
       <Box className="text-white">this is archive page</Box>
+      <Navbar />
     </Box>
   );
 }
--- frontend/src/UI/pages/Lobby.jsx
+++ frontend/src/UI/pages/Lobby.jsx
@@ -3,7 +3,7 @@ import Navbar from "../organisms/Navbar";
 
 function Lobby() {
   return (
-    <Box className="bg-base2 h-screen">
+    <Box className="bg-base2 h-screen text-white">
       <Box className="text-white">
         <Navbar />
       </Box>
