9649114b2b66ed6a48dab7eece83fa4359ace4ed
yoonsun <jyoonsun0217@gmail.com>
2022-10-25 14:10:04 +0900

    [FE] feat: add start btn
    Former-commit-id: 2f11b9ccb926fcf2aca1bf2787c382d9c9740da6

--- frontend/src/UI/atoms/NeonLogo.jsx
+++ frontend/src/UI/atoms/NeonLogo.jsx
@@ -1,6 +1,7 @@
 import { Box, Container, Text } from "@chakra-ui/react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Login from "../pages/Login";
 
 function NeonLogo() {
   const [move, setMove] = useState(false);
@@ -23,7 +24,7 @@ function NeonLogo() {
             }
           >
             <Text
-              className="flex items-center justify-center w-full font-neon text-[6.5em] text-[#f1f5ff] whitespace-pre drop-shadow-3xl animate-flicker before:content-[''] before:absolute before:h-[6px] before:-top-4 before:left-1/2 before:transform before:-translate-x-1/2 before:w-full before:bg-[#fffcd7] before:border before:rounded-lg before:shadow-4xl
+              className="flex items-center justify-center w-full font-neon text-[5.5em] text-[#f1f5ff] whitespace-pre drop-shadow-3xl animate-flicker before:content-[''] before:absolute before:h-[6px] before:-top-4 before:left-1/2 before:transform before:-translate-x-1/2 before:w-full before:bg-[#fffcd7] before:border before:rounded-lg before:shadow-4xl
             after:content-[''] after:absolute after:h-[6px] after:-bottom-4 after:left-1/2 after:transform after:-translate-x-1/2 after:w-full after:bg-[#fffcd7] after:border-spacing-2 after:rounded-lg after:shadow-4xl"
             >
               RE-VERSE
@@ -34,19 +35,19 @@ function NeonLogo() {
       </Container>
       <Container className="absolute grid place-items-center">
         <Box>
-          {/* <Link to="/login" element={<Login />}> */}
-          {/* // FIXME: delay가 안먹는 이슈 고칠 것 */}
-          <Text
-            className={
-              `absolute top-1/3 left-auto -mt-7 mb-0 -ml-36 font-neon text-[6.5em] z-10 text-[#f890e7] bg-transparent border-none drop-shadow-4xl` +
-              (start
-                ? ` visible delay-[5000ms]`
-                : ` hidden opacity-0 transition-opacity delay-500 ease-linear`)
-            }
-          >
-            START
-          </Text>
-          {/* </Link> */}
+          <Link to="/login" element={<Login />}>
+            {/* // FIXME: delay가 안먹는 이슈 고칠 것 */}
+            <Text
+              className={
+                `absolute top-1/3 left-auto -mt-7 mb-0 -ml-36 font-neon text-[6.5em] z-10 text-[#f890e7] bg-transparent border-none drop-shadow-4xl` +
+                (start
+                  ? ` visible delay-[5000ms]`
+                  : ` hidden opacity-0 transition-opacity delay-500 ease-linear`)
+              }
+            >
+              START
+            </Text>
+          </Link>
         </Box>
       </Container>
     </Box>
