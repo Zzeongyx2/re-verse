f06dc2052c3098671992ffb58ef3bb9db0ab90e6
yoonsun <jyoonsun0217@gmail.com>
2022-10-27 09:36:50 +0900

    [FE] fix: breadcrumbs - complete
    Former-commit-id: 07fbefc01040aee1cfb77c0525f0ce677b3c96c7

--- frontend/src/UI/organisms/Navbar.jsx
+++ frontend/src/UI/organisms/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom";
 import {
   Box,
   Breadcrumb,
@@ -6,34 +7,66 @@ import {
   BreadcrumbSeparator,
 } from "@chakra-ui/react";
 
+import { Link } from "@chakra-ui/react";
+import { useEffect } from "react";
+
 function Navbar() {
+  const location = useLocation();
+  useEffect(() => {
+    console.log(location);
+    console.log(location.pathname);
+  }, [location]);
   return (
     <Box>
       <Box>
-        {/* <Breadcrumb>
+        <Breadcrumb>
           <BreadcrumbItem>
-            <BreadcrumbLink as={Link} to="/lobby">
-              RE-VERSE
-            </BreadcrumbLink>
+            <BreadcrumbLink href="/lobby">RE-VERSE</BreadcrumbLink>
           </BreadcrumbItem>
-          <BreadcrumbItem>
-            <BreadcrumbLink isCurrentPage as={Link} to="/character">
-              캐릭터 선택
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <BreadcrumbLink isCurrentPage as={Link} to="/friend">
-              친구
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <BreadcrumbLink isCurrentPage as={Link} to="/archive">
-              아카이브
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb> */}
+          {location.pathname === "/character" && (
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/character">캐릭터 선택</BreadcrumbLink>
+            </BreadcrumbItem>
+          )}
+          {location.pathname === "/friend" && (
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/friend">친구</BreadcrumbLink>
+            </BreadcrumbItem>
+          )}
+          {location.pathname === "/archive" && (
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/archive">아카이브</BreadcrumbLink>
+            </BreadcrumbItem>
+          )}
+        </Breadcrumb>
       </Box>
     </Box>
+    // <Box>
+    //   <Box>
+    //     <Breadcrumb>
+    //       <BreadcrumbItem>
+    //         <BreadcrumbLink as={Link} to="/lobby">
+    //           RE-VERSE
+    //         </BreadcrumbLink>
+    //       </BreadcrumbItem>
+    //       <BreadcrumbItem>
+    //         <BreadcrumbLink as={Link} to="/character">
+    //           캐릭터 선택
+    //         </BreadcrumbLink>
+    //       </BreadcrumbItem>
+    //       <BreadcrumbItem>
+    //         <BreadcrumbLink as={Link} to="/friend">
+    //           친구
+    //         </BreadcrumbLink>
+    //       </BreadcrumbItem>
+    //       <BreadcrumbItem>
+    //         <BreadcrumbLink isCurrentPage as={Link} to="/archive">
+    //           아카이브
+    //         </BreadcrumbLink>
+    //       </BreadcrumbItem>
+    //     </Breadcrumb>
+    //   </Box>
+    // </Box>
   );
 }
 
