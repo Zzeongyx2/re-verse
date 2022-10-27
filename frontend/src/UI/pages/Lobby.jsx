8de7462472ced8912abedac414e2f05913a12ed7
yoonsun <jyoonsun0217@gmail.com>
2022-10-27 15:58:18 +0900

    [FE] fix: update lobby components
    Former-commit-id: ce038c5aecccb6402e1c090677c9248ded9a7499

--- frontend/src/UI/pages/Lobby.jsx
+++ frontend/src/UI/pages/Lobby.jsx
@@ -1,4 +1,7 @@
 import { Box } from "@chakra-ui/react";
+import LobbyButton from "../atoms/LobbyButton";
+import NeonLightBG from "../atoms/NeonLightBG";
+import LobbyProfile from "../organisms/LobbyProfile";
 import Navbar from "../organisms/Navbar";
 
 function Lobby() {
@@ -7,6 +10,26 @@ function Lobby() {
       <Box className="">
         <Navbar />
       </Box>
+      <NeonLightBG />
+      {/* // FIXME: 나중에 linkto 속성값 변경해야 함 */}
+      <LobbyButton
+        linkTo={"/lobby"}
+        buttonTitle={"대표 아카이브 바로가기"}
+        buttonMessage={"zl존윤sun의 메인 아카이브"}
+        textcolor={"text-white"}
+        from={"from-main1"}
+        to={"to-sub1"}
+      />
+      <br />
+      <LobbyButton
+        linkTo={"/lobby"}
+        buttonTitle={"최근 방문한 아카이브 바로가기"}
+        buttonMessage={"zl존윤sun의 메인 아카이브"}
+        textcolor={"text-gray-700"}
+        from={"from-main2"}
+        to={"to-sub2"}
+      />
+      <LobbyProfile />
     </Box>
   );
 }
