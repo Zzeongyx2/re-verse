76c00be4775a5dba7e0a9beb7daf9fc255ca239e
yoonsun <jyoonsun0217@gmail.com>
2022-10-27 13:48:57 +0900

    [FE] fix: delete bg styles
    Former-commit-id: bb2d8d3819f1da4f0d5cc788da8ef186269df2a6

--- frontend/src/UI/pages/Archive.jsx
+++ frontend/src/UI/pages/Archive.jsx
@@ -3,9 +3,9 @@ import Navbar from "../organisms/Navbar";
 
 function Archive() {
   return (
-    <Box className="bg-base2 h-screen text-white">
-      <Box className="text-white">this is archive page</Box>
+    <Box>
       <Navbar />
+      <Box className="text-white">this is archive page</Box>
     </Box>
   );
 }
