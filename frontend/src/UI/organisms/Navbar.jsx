d75c0e903265c8285992e6b899a6fe62fe851612
yoonsun <jyoonsun0217@gmail.com>
2022-10-27 13:48:21 +0900

    [FE] fix: update icons
    Former-commit-id: 246b3fc8679e54d04b735001d80a980a7652f60e

--- frontend/src/UI/organisms/Navbar.jsx
+++ frontend/src/UI/organisms/Navbar.jsx
@@ -1,14 +1,14 @@
 import { useLocation } from "react-router-dom";
-import {
-  Box,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator,
-} from "@chakra-ui/react";
+import { Box, Breadcrumb, BreadcrumbItem } from "@chakra-ui/react";
 
-import { Link } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { useEffect } from "react";
+import BreadCrumbBtn from "../atoms/BreadCrumbBtn";
+
+import NavBtn from "../atoms/NavBtn";
+
+import { AiFillHome } from "react-icons/ai";
+import { MdPeopleAlt } from "react-icons/md";
 
 function Navbar() {
   const location = useLocation();
@@ -17,29 +17,94 @@ function Navbar() {
     console.log(location.pathname);
   }, [location]);
   return (
-    <Box>
+    <Box className="flex justify-between">
       <Box>
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/lobby">RE-VERSE</BreadcrumbLink>
-          </BreadcrumbItem>
-          {location.pathname === "/character" && (
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/character">캐릭터 선택</BreadcrumbLink>
-            </BreadcrumbItem>
-          )}
-          {location.pathname === "/friend" && (
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/friend">친구</BreadcrumbLink>
-            </BreadcrumbItem>
-          )}
-          {location.pathname === "/archive" && (
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/archive">아카이브</BreadcrumbLink>
-            </BreadcrumbItem>
-          )}
+        <Breadcrumb separator="">
+          <Box className="relative">
+            <Box className="absolute z-50">
+              <BreadcrumbItem>
+                <Link to="/lobby">
+                  <BreadCrumbBtn
+                    text={"RE-VERSE"}
+                    from={"from-main1"}
+                    to={"to-sub1"}
+                  />
+                </Link>
+              </BreadcrumbItem>
+            </Box>
+            <Box className="absolute left-52">
+              {location.pathname === "/lobby" && (
+                <BreadcrumbItem>
+                  <Link to="/lobby">
+                    <BreadCrumbBtn
+                      text={"로비"}
+                      from={"from-main2"}
+                      to={"to-sub2"}
+                    />
+                  </Link>
+                </BreadcrumbItem>
+              )}
+              {location.pathname === "/character" && (
+                <BreadcrumbItem>
+                  <Link to="/character">
+                    <BreadCrumbBtn
+                      text={"캐릭터 선택"}
+                      from={"from-main2"}
+                      to={"to-sub2"}
+                    />
+                  </Link>
+                </BreadcrumbItem>
+              )}
+              {location.pathname === "/friend" && (
+                <BreadcrumbItem>
+                  <Link to="/friend">
+                    <BreadCrumbBtn
+                      text={"친구"}
+                      from={"from-main2"}
+                      to={"to-sub2"}
+                    />
+                  </Link>
+                </BreadcrumbItem>
+              )}
+              {location.pathname === "/archive" && (
+                <BreadcrumbItem>
+                  <Link to="/archive">
+                    <BreadCrumbBtn
+                      text={"아카이브"}
+                      from={"from-main2"}
+                      to={"to-sub2"}
+                    />
+                  </Link>
+                </BreadcrumbItem>
+              )}
+            </Box>
+          </Box>
         </Breadcrumb>
       </Box>
+      {/* <Box className=""> */}
+      <Box className="flex justify-between w-72">
+        <NavBtn
+          icon={<AiFillHome className="text-3xl text-white" />}
+          from={"from-main1"}
+          to={"to-sub1"}
+        />
+        <NavBtn
+          icon={<MdPeopleAlt className="text-3xl text-white" />}
+          from={"from-main1"}
+          to={"to-sub1"}
+        />
+        <NavBtn
+          icon={<AiFillHome className="text-3xl text-white" />}
+          from={"from-main1"}
+          to={"to-sub1"}
+        />
+        <NavBtn
+          icon={<AiFillHome className="text-3xl text-white" />}
+          from={"from-main1"}
+          to={"to-sub1"}
+        />
+      </Box>
+      {/* </Box> */}
     </Box>
     // <Box>
     //   <Box>
