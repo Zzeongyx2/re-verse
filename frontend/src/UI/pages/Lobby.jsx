85e032182d77523fc2a79cfac70d84e2229ad9fe
yoonsun <jyoonsun0217@gmail.com>
2022-10-27 17:54:24 +0900

    [FE] fix: update lobby profile codes
    Former-commit-id: 672052c42fd94dd6d2339380c03578b93ff3fb33

--- frontend/src/UI/pages/Lobby.jsx
+++ frontend/src/UI/pages/Lobby.jsx
@@ -3,13 +3,36 @@ import NeonLightBG from "../atoms/NeonLightBG";
 import LobbyProfile from "../organisms/LobbyProfile";
 import Navbar from "../organisms/Navbar";
 
+import ProfileTest from "../organisms/ProfileTest";
+
 function Lobby() {
   return (
     <div className="mt-8">
       <div className="">
         <Navbar />
-      </div>
-    </div>
+      </Box>
+      <NeonLightBG />
+      {/* // FIXME: 나중에 linkto 속성값 변경해야 함 */}
+      <LobbyButton
+        linkTo={"/lobby"}
+        buttonTitle={"대표 아카이브 바로가기"}
+        buttonMessage={"zl존윤sun의 메인 아카이브"}
+        textcolor={"text-white"}
+        from={"from-main1"}
+        to={"to-sub1"}
+      />
+      <br />
+      <LobbyButton
+        linkTo={"/lobby"}
+        buttonTitle={"최근 방문한 아카이브 바로가기"}
+        buttonMessage={"zl존윤sun의 메인 아카이브"}
+        textcolor={"text-gray-700"}
+        from={"from-main2"}
+        to={"to-sub2"}
+      />
+      <ProfileTest />
+      <LobbyProfile />
+    </Box>
   );
 }
 
