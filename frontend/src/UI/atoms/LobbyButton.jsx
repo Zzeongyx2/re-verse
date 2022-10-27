29ee663677c0ac9da66ce418c378e45858dea0f9
yoonsun <jyoonsun0217@gmail.com>
2022-10-27 15:58:32 +0900

    [FE] fix: lobbybutton props
    Former-commit-id: 9af318c974b4201ecf106993409fab4438f328bb

--- frontend/src/UI/atoms/LobbyButton.jsx
+++ frontend/src/UI/atoms/LobbyButton.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import { Box, Grid, GridItem } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-function LobbyButton({ linkTo, textcolor, backcolor, buttonTitle, buttonMessage }) {
+function LobbyButton({
+  linkTo,
+  textcolor,
+  buttonTitle,
+  buttonMessage,
+  from,
+  to,
+}) {
   return (
     <Link to={linkTo}>
       <Box
-        backgroundColor={backcolor}
         border="1px"
         borderColor="white"
-        color={textcolor}
         borderRadius="24"
         p={5}
         textAlign="end"
-        className="h-[90px] w-[545px]"
+        className={`h-[90px] w-full ${textcolor} bg-gradient-to-t ${from} ${to}`}
       >
         <Grid templateColumns="repeat(5)" templateRows="repeat(2)" gap={1}>
           <GridItem colSpan={5} rowSpan={1}>
-            <div className="text-sm drop-shadow">{buttonTitle}</div>
+            <div className="font-semibold text-sm drop-shadow">
+              {buttonTitle}
+            </div>
           </GridItem>
           <GridItem colSpan={5} rowSpan={1}>
             <div className="font-bold text-lg drop-shadow">{buttonMessage}</div>
