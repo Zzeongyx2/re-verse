0aec2e9564cb5768f5530572a2f4de6747d14f44
yoonsun <jyoonsun0217@gmail.com>
2022-10-27 13:47:07 +0900

    [FE] fix: update entire style
    Former-commit-id: d110033a126aa346ae9dad063ff6dde1fd8f574e

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,26 @@ import Login from "./UI/pages/Login";
 import Signin from "./UI/pages/Signin";
 import TestPage from "./UI/organisms/TestPage";
 import Lobby from "./UI/pages/Lobby";
-import SelectCharacter from "./UI/pages/SelectCharacter";
+import Character from "./UI/pages/Character";
 import Friend from "./UI/pages/Friend";
 import Archive from "./UI/pages/Archive";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NonLoginMain />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/test" element={<TestPage />} />
-        <Route path="/lobby" element={<Lobby />} />
-        <Route path="/character" element={<SelectCharacter />} />
-        <Route path="/friend" element={<Friend />} />
-        <Route path="/archive" element={<Archive />} />
-      </Routes>
+      <Box className="bg-base2 h-screen mx-auto px-14 my-auto py-10 font-sans">
+        <Routes>
+          <Route path="/" element={<NonLoginMain />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/test" element={<TestPage />} />
+          <Route path="/lobby" element={<Lobby />} />
+          <Route path="/character" element={<Character />} />
+          <Route path="/friend" element={<Friend />} />
+          <Route path="/archive" element={<Archive />} />
+        </Routes>
+      </Box>
     </BrowserRouter>
   );
 }
