655318956caa27b144c1fb41edb1f6359df0a95e
지용현 <yung5487@gmail.com>
2022-10-27 16:30:56 +0900

    [FE] feat: create friend list,accept organisms
    Former-commit-id: ffd5441324de8b9d68e9a9d323a6cd6f06eb4c02

--- frontend/src/UI/organisms/FriendRequest.jsx
+++ frontend/src/UI/organisms/FriendRequest.jsx
@@ -1,49 +1,24 @@
 import { useState, useEffect } from "react";
 import { Box, Grid, GridItem, Input } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
-import { FiMinusCircle } from "react-icons/fi";
-import { BiLogIn } from "react-icons/bi";
-import { HiOutlineTrash } from "react-icons/hi";
+import { FiPlusCircle } from "react-icons/fi";
 
-function FriendList() {
+function FriendRequest() {
   const [findNickName, setFindNickName] = useState("");
-  const [friendList, setFriendList] = useState([]);
-  const [archiveList, setArchiveList] = useState([]);
-  const [selectFriend, setSelectFriend] = useState({
-    email: "",
-    nickname: "",
-    avatar: "",
-    message: "",
-  });
-  const [rightTitle, setRightTitle] = useState("");
+  const [userList, setUserList] = useState([]);
 
   const findNickNameHandleChange = (e) => {
     setFindNickName(e.target.value);
   };
 
-  const friendDelete = (email) => {
-    // TODO: 친구삭제 하기
-    console.log("친구삭제", email);
-  };
-
-  const clickNickname = (friend) => {
-    setSelectFriend(friend);
-    setRightTitle(`나와함께하는 '${friend.nickname}'의 아카이브`);
-  };
-
-  const enterArchive = (archiveId) => {
-    // TODO: 아카이브로 이동
-    console.log(archiveId, "이동");
-  };
-
-  const archiveDelete = (archiveId) => {
-    // TODO: 아카이브 삭제
-    console.log(archiveId, "삭제");
+  const friendRequest = (email) => {
+    // TODO: 친구요청 하기
+    console.log("친구요청", email);
   };
 
   useEffect(() => {
-    // TODO: axios 요청으로 친구 목록 받기
-    setFriendList((list) => {
+    // TODO: axios 요청으로 유저 목록 받기
+    setUserList((list) => {
       return [
         ...list,
         {
@@ -70,46 +45,7 @@ function FriendList() {
         },
       ];
     });
-    if (friendList.length > 0) {
-      setSelectFriend(friendList[0]);
-      setRightTitle(`나와함께하는 '${friendList[0].nickname}'의 아카이브`);
-    } else {
-      setRightTitle("함께하는 친구가 없습니다");
-    }
   }, []);
-
-  useEffect(() => {
-    // TODO: 선택 유저 바뀔때마다 아카이브 목록 가져오기
-    console.log(selectFriend.nickname, "아카이브 목록 가져옴");
-    setArchiveList([
-      {
-        archiveId: "a1",
-        title: "놀러와요 KAN쵸월드",
-        description: "하루에 3번, 3분씩 3개의 칸쵸를 먹자",
-        level: 1,
-        bookmarks: false,
-        members: [
-          {
-            nickname: "name",
-            avatar: "aaa",
-          },
-        ],
-      },
-      {
-        archiveId: "a2",
-        title: "놀러와요 KAN쵸월드 (2)",
-        description: "하루에 6번, 6분씩 6개의 칸쵸를 먹자",
-        level: 1,
-        bookmarks: true,
-        members: [
-          {
-            nickname: "String",
-            avatar: "112",
-          },
-        ],
-      },
-    ]);
-  }, [selectFriend]);
 
   return (
     <Box>
@@ -122,7 +58,7 @@ function FriendList() {
             <label className="flex justify-center items-center pl-4">
               <BsSearch />
               <Input
-                placeholder="닉네임을 검색하여 친구를 찾아보세요"
+                placeholder="닉네임을 검색하여 유저를 찾아보세요"
                 focusBorderColor="none"
                 border={0}
                 onChange={findNickNameHandleChange}
@@ -131,7 +67,7 @@ function FriendList() {
             </label>
           </Box>
           <Box className="overflow-y-scroll h-[400px]">
-            {friendList
+            {userList
               .filter((friend) => {
                 if (findNickName.trim() === "") {
                   return friend;
@@ -153,23 +89,16 @@ function FriendList() {
                       <img src={friend.avatar} alt={friend.nickname} className="w-full h-full" />
                     </GridItem>
                     <GridItem colSpan={13} rowSpan={1} className="text-xs font-bold">
-                      <div
-                        className="hover:cursor-pointer"
-                        onClick={() => {
-                          clickNickname(friend);
-                        }}
-                      >
-                        {friend.nickname}
-                      </div>
+                      <div>{friend.nickname}</div>
                     </GridItem>
                     <GridItem colStart={15} rowSpan={2} className="w-5 h-5 mr-2">
                       <button
                         className="w-full h-full"
                         onClick={() => {
-                          friendDelete(friend.email);
+                          friendRequest(friend.email);
                         }}
                       >
-                        <FiMinusCircle className="text-red-500 w-full h-full" />
+                        <FiPlusCircle className="text-[#3F81FB] w-full h-full" />
                       </button>
                     </GridItem>
                     <GridItem
@@ -187,59 +116,10 @@ function FriendList() {
         <GridItem
           colSpan={1}
           className="bg-white rounded-3xl w-[43vw] h-full py-4 px-5 place-self-end"
-        >
-          <Box className="shadow p-2 font-bold text-xl whitespace-nowrap overflow-hidden text-ellipsis">
-            {rightTitle}
-          </Box>
-          <Box className="overflow-y-scroll h-[400px]">
-            {archiveList.map((archive, index) => {
-              return (
-                <Grid
-                  key={index}
-                  templateColumns="repeat(15, 1fr)"
-                  templateRows="repeat(2, 1fr)"
-                  alignItems="center"
-                  gap={2}
-                  className="my-1 py-1 shadow"
-                >
-                  <GridItem colSpan={13} rowSpan={1} className="text-xs font-bold ">
-                    {archive.title}
-                  </GridItem>
-                  <GridItem colStart={14} colSpan={1} rowSpan={2} className="w-7 h-7">
-                    <button
-                      className="w-full h-full bg-[#15B9F1] rounded-3xl border-4 border-[#B7C6E7]"
-                      onClick={() => {
-                        enterArchive(archive.archiveId);
-                      }}
-                    >
-                      <BiLogIn className="text-white w-4 h-4" />
-                    </button>
-                  </GridItem>
-                  <GridItem colStart={15} colSpan={1} rowSpan={2} className="w-7 h-7 mr-2">
-                    <button
-                      className="w-full h-full bg-[#FF7067] rounded-3xl border-4 border-[#B7C6E7] flex items-center justify-center"
-                      onClick={() => {
-                        archiveDelete(archive.archiveId);
-                      }}
-                    >
-                      <HiOutlineTrash className="text-white w-4 h-4" />
-                    </button>
-                  </GridItem>
-                  <GridItem
-                    colSpan={13}
-                    rowSpan={1}
-                    className="text-xs whitespace-nowrap overflow-hidden text-ellipsis"
-                  >
-                    {archive.description}
-                  </GridItem>
-                </Grid>
-              );
-            })}
-          </Box>
-        </GridItem>
+        ></GridItem>
       </Grid>
     </Box>
   );
 }
 
-export default FriendList;
+export default FriendRequest;
