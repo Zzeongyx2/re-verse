7b92aaeb96a57f911836a72c4f50f595c969a2a0
YunYoung <jeongyy98>
2022-10-27 10:47:40 +0900

    [BE] settings: folder structure
    Former-commit-id: 1eb1810bd4ea53379bdce7e437a3d648d21f4f2d

---
+++ backend/auth/src/main/java/kr/co/reverse/auth/api/controller/AuthController.java
@@ -0,0 +1,17 @@
+package kr.co.reverse.auth.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi/auth")
+public class AuthController {
+
+    //TODO : 회원가입
+
+    //TODO : 로그인
+
+    //TODO : 로그아웃
+
+    //TODO : 탈퇴하기기
+}
