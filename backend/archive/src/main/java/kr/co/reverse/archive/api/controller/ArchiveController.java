5145a45dd8805d95132d068f18c4192acee10fa1
kancho1216 <kancho1216@naver.com>
2022-10-28 17:53:24 +0900

    [BE] fix: primary key with uuid
    Former-commit-id: e10fd250e573e868a6c21f5de7195b08e2efa50c

--- backend/archive/src/main/java/kr/co/reverse/archive/api/controller/ArchiveController.java
+++ backend/archive/src/main/java/kr/co/reverse/archive/api/controller/ArchiveController.java
@@ -5,10 +5,7 @@ import kr.co.reverse.archive.api.service.ArchiveService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/archive")
@@ -24,4 +21,9 @@ public class ArchiveController {
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+//    @GetMapping
+//    public ResponseEntity<? extends > getArchives() {
+//        archiveService
+//    }
 }
--- backend/archive/src/main/java/kr/co/reverse/archive/api/service/ArchiveService.java
+++ backend/archive/src/main/java/kr/co/reverse/archive/api/service/ArchiveService.java
@@ -2,11 +2,14 @@ package kr.co.reverse.archive.api.service;
 
 import kr.co.reverse.archive.api.request.ArchiveReq;
 import kr.co.reverse.archive.db.entity.Archive;
+import kr.co.reverse.archive.db.entity.User;
 import kr.co.reverse.archive.db.repository.ArchiveRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +17,7 @@ public class ArchiveService {
 
     private final ArchiveRepository archiveRepository;
 
+    @Transactional
     public void createArchive(ArchiveReq archiveReq) {
         String title = archiveReq.getTitle();
         String description = archiveReq.getDescription();
@@ -31,5 +35,10 @@ public class ArchiveService {
                 .build();
 
         archiveRepository.save(archive);
+    }
+
+    public List<Archive> getArchives(User user) {
+//        return archiveRepository.findAllByUser(user);
+        return null;
     }
 }
