0ffbbf91622a0b373c524b534a6ea2051278dc5e
kancho1216 <kancho1216@naver.com>
2022-10-28 17:53:24 +0900

    [BE] fix: primary key with uuid
    Former-commit-id: 068084a557f8dd8254631b8acf679eb251674a75

--- backend/archive/src/main/java/kr/co/reverse/archive/db/repository/ArchiveRepository.java
+++ backend/archive/src/main/java/kr/co/reverse/archive/db/repository/ArchiveRepository.java
@@ -1,8 +1,13 @@
 package kr.co.reverse.archive.db.repository;
 
 import kr.co.reverse.archive.db.entity.Archive;
+import kr.co.reverse.archive.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArchiveRepository extends JpaRepository<Archive, Integer> {
-    
+
+//    List<Archive> findAllByUser(User user);
+
 }
