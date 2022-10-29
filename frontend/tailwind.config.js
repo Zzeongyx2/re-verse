056711e51e06f0d1ee87c95dd155c70fd05dcf1d
yoonsunny17 <jyoonsun0217@gmail.com>
2022-10-29 12:13:18 +0900

    [FE] feat: neon logo test
    Former-commit-id: 3826e0d1b8a23a007dbb77ef4afa64fccd3e87fa

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -27,15 +27,24 @@ module.exports = {
       dropShadow: {
         "3xl": [
           "0 0 0 transparent",
-          "0 0 10px #0049ff",
-          "0 0 20px rgba(0, 73, 255, 0.5)",
-          "0 0 40px #0049ff",
-          "0 0 300px #0049ff",
-          "0 0 200px #0049ff",
-          "0 0 300px #0049ff",
-          "0 0 500px #0049ff",
+          // "0 0 0px #0044ed",
+          "0 0 2px #0049ff",
+          "0 0 5px rgba(0, 73, 255, 0.5)",
+          "0 0 4px #0049ff",
+          // "0 0 6px #0049ff",
+          "0 0 6px rgba(0, 73, 255, 0.5)",
         ],
-        "4xl": ["0 0 0.125em #fa61bf", "0 0 0.45em #fa61bf"],
+        // "3xl": [
+        //   "0 0 0 transparent",
+        //   "0 0 10px #0049ff",
+        //   "0 0 20px rgba(0, 73, 255, 0.5)",
+        //   "0 0 40px #0049ff",
+        //   "0 0 300px #0049ff",
+        //   "0 0 200px #0049ff",
+        //   "0 0 300px #0049ff",
+        //   "0 0 500px #0049ff",
+        // ],
+        "4xl": ["0 0 2px #fa61bf", "0 0 6px #fa61bf"],
         "5xl": [
           "0 0 0 transparent",
           "0 0 10px #0049ff",
@@ -49,6 +58,7 @@ module.exports = {
       animation: {
         flicker: "flicker 5s infinite alternate",
         blinking: "blinking 0.01s infinite alternate",
+        blinking2: "blinking 0.02s infinite alternate",
       },
       keyframes: {
         flicker: {
@@ -63,7 +73,7 @@ module.exports = {
       },
       boxShadow: {
         "3xl":
-          "inset 0 0 22px 3px rgba(72, 120, 239, 0.35), 0 0 28px 5px rgba(72, 120, 239, 0.45)",
+          "inset 0 0 22px 3px rgba(0, 73, 255, 0.35), 0 0 20px 1px rgba(72, 120, 239, 0.5), 0 0 28px 5px rgba(0, 73, 255, 0.5)",
 
         "4xl":
           "inset 0 0 18px rgba(251, 255, 0, 0.35), 0 0 18px 5px rgba(251, 255, 0, 0.35)",
