7130ca89497b692f8cf144873d50591678f3a373
yoonsunny17 <jyoonsun0217@gmail.com>
2022-10-29 12:13:18 +0900

    [FE] feat: neon logo test
    Former-commit-id: 0a43cd34e167c0d7bfbd37394dd0644855f01811

---
+++ frontend/src/UI/pages/NeonTest.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import NeonLogoTest from "../atoms/NeonLogoTest";
+
+function NeonTest() {
+  return (
+    <Box>
+      <NeonLogoTest />
+    </Box>
+  );
+}
+
+export default NeonTest;
