b465b2ea5c916674df3a8d1e8f29e827e00306ca
yoonsunny17 <jyoonsun0217@gmail.com>
2022-10-29 20:02:22 +0900

    [FE] fix: resize navigation bar
    Former-commit-id: a6e5b3192e38b539ddc4429913caaeda9a693725

--- frontend/src/UI/atoms/BreadCrumbBtn.jsx
+++ frontend/src/UI/atoms/BreadCrumbBtn.jsx
@@ -1,7 +1,7 @@
 function BreadCrumbBtn({ text, to, from }) {
   return (
     <button
-      className={`text-white font-bold border border-1 border-white text-3xl rounded-l-lg rounded-r-[50px] w-60 h-16 text-center bg-gradient-to-t ${from} ${to}`}
+      className={`text-white font-bold border border-1 border-white text-2xl rounded-l-lg rounded-r-[50px] w-52 h-14  text-center bg-gradient-to-t ${from} ${to}`}
     >
       {text}
     </button>
