ab98b94428497eeff3536820bd84ebd9e39535b9
yoonsunny17 <jyoonsun0217@gmail.com>
2022-10-30 23:51:58 +0900

    [FE] fix: friend request
    Former-commit-id: 6e37486172544fbc84a83cdca63617a8d21bf1c6

--- frontend/src/UI/organisms/FriendRequest.jsx
+++ frontend/src/UI/organisms/FriendRequest.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
-import { Box, Grid, GridItem, Input } from "@chakra-ui/react";
+
 import { BsSearch } from "react-icons/bs";
 import { FiPlusCircle } from "react-icons/fi";
+
+import { Avatar, Divider } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Input } from "@chakra-ui/react";
 
 function FriendRequest() {
   const [findNickName, setFindNickName] = useState("");
   const [userList, setUserList] = useState([]);
   // TODO: 이미지 저장용 변수 나중에 지우기
-  const [image] = useState(
-    "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/5f2b4e0f-cd21-46d7-a5c3-b392a363d398/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221028%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221028T014603Z&X-Amz-Expires=86400&X-Amz-Signature=5cfd5ce99324a5c83710ee8d825ca12caa1c23a33835352bb9d5fe12fa2a2d0c&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject"
+  const [profileImg] = useState(
+    "https://images.unsplash.com/photo-1628260412297-a3377e45006f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
   );
+
   // TODO:
   const findNickNameHandleChange = (e) => {
     setFindNickName(e.target.value);
@@ -28,19 +32,19 @@ function FriendRequest() {
         {
           email: "email@naver.com",
           nickname: "KIN거운KAN쵸",
-          avatar: image,
+          avatar: profileImg,
           message: "엉망으로 살아야 해! 인생은 한 번이야!",
         },
         {
           email: "email@naver.com",
           nickname: "oO강약약강Oo",
-          avatar: image,
+          avatar: profileImg,
           message: "가는 말이 고우면, 얕본다.",
         },
         {
           email: "email@naver.com",
           nickname: "zl존윤sun2",
-          avatar: image,
+          avatar: profileImg,
           message:
             "새벽에 먹는 맥주와 치킨은 0 칼로리다.새벽에 먹는 맥주와 치킨은 0 칼로리다.새벽에 먹는 맥주와 치킨은 0 칼로리다.",
         },
@@ -49,77 +53,75 @@ function FriendRequest() {
   }, []);
 
   return (
-    <Box>
-      <Grid templateColumns="repeat(2, 1fr)" justifyItems="center" className="h-[500px]">
-        <GridItem
-          colSpan={1}
-          className="bg-white rounded-3xl w-[43vw] h-full py-4 px-5 place-self-start"
-        >
-          <Box className="shadow ">
-            <label className="flex justify-center items-center pl-4">
-              <BsSearch />
-              <Input
-                placeholder="닉네임을 검색하여 유저를 찾아보세요"
-                focusBorderColor="none"
-                border={0}
-                onChange={findNickNameHandleChange}
-                value={findNickName}
-              />
-            </label>
-          </Box>
-          <Box className="overflow-y-scroll h-[400px]">
+    <div className="text-base2">
+      <div className="flex justify-between">
+        {/* friend request */}
+        <div className="bg-white rounded-3xl w-[calc(96%/2)] h-[600px] pt-5 pb-6 flex flex-col items-center">
+          <div className="w-[calc(100%-70px)] flex justify-center items-center px-4 py-2 mb-3 border-2 border-base1/20 rounded-2xl">
+            <BsSearch size={24} />
+            <input
+              type="text"
+              placeholder="닉네임을 검색하여 친구를 찾아보세요"
+              onChange={findNickNameHandleChange}
+              value={findNickName}
+              className="w-full focus:outline-none pl-3.5 text-sm"
+            />
+          </div>
+          {/* user lifo */}
+          <div className="w-[calc(100%-70px)] overflow-auto scrollbar-hide">
             {userList
               .filter((friend) => {
                 if (findNickName.trim() === "") {
-                  return friend;
+                  return friendRequest;
                 } else if (friend.nickname.includes(findNickName)) {
                   return friend;
                 }
               })
               .map((friend, index) => {
                 return (
-                  <Grid
-                    key={index}
-                    templateColumns="repeat(15, 1fr)"
-                    templateRows="repeat(2, 1fr)"
-                    alignItems="center"
-                    gap={2}
-                    className="my-1 py-1 shadow"
-                  >
-                    <GridItem colSpan={1} rowSpan={2} className="w-9 h-9">
-                      <img src={friend.avatar} alt={friend.nickname} className="w-full h-full" />
-                    </GridItem>
-                    <GridItem colSpan={13} rowSpan={1} className="text-xs font-bold">
-                      <div>{friend.nickname}</div>
-                    </GridItem>
-                    <GridItem colStart={15} rowSpan={2} className="w-5 h-5 mr-2">
+                  <div>
+                    <div
+                      key={index}
+                      className="flex items-center justify-between px-2 py-1"
+                    >
+                      <div className="flex items-center">
+                        <Avatar name="profileImg" src={profileImg} size="sm" />
+                        <div className="text-base1 px-3">
+                          <p className="cursor-pointer text-sm font-bold">
+                            {friend.nickname}
+                          </p>
+                          <p className="overflow-hidden text-ellipsis line-clamp-1 text-xs text-zinc-500">
+                            {friend.message}
+                          </p>
+                          {/* <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+                        {friend.message}
+                      </div> */}
+                        </div>
+                      </div>
                       <button
-                        className="w-full h-full"
                         onClick={() => {
                           friendRequest(friend.email);
                         }}
                       >
-                        <FiPlusCircle className="text-[#3F81FB] w-full h-full" />
+                        <FiPlusCircle className="text-[#3F81FB]" size={20} />
                       </button>
-                    </GridItem>
-                    <GridItem
-                      colSpan={13}
-                      rowSpan={1}
-                      className="text-xs whitespace-nowrap overflow-hidden text-ellipsis"
-                    >
-                      {friend.message}
-                    </GridItem>
-                  </Grid>
+                    </div>
+                    <Divider />
+                  </div>
                 );
               })}
-          </Box>
-        </GridItem>
-        <GridItem
-          colSpan={1}
-          className="bg-white rounded-3xl w-[43vw] h-full py-4 px-5 place-self-end"
-        ></GridItem>
-      </Grid>
-    </Box>
+          </div>
+        </div>
+        <div className="bg-white rounded-3xl w-[calc(96%/2)] h-[600px] pt-5 pb-6 flex flex-col items-center justify-center">
+          <p className="font-bold text-3xl mb-6">사랑~해요~</p>
+          <img
+            className="aspect-square rounded-full w-3/5"
+            src="https://images.velog.io/images/sdp1123/post/ea263380-9ae0-4850-bd69-6af4dcf3e8e6/%EB%9A%B1%EC%9D%B41.jpg"
+            alt="nothing"
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
