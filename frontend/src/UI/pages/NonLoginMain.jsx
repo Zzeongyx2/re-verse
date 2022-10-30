f44886b74df0447569b668f9761561847d4437c0
yoonsunny17 <jyoonsun0217@gmail.com>
2022-10-30 21:29:44 +0900

    [FE] fix: update layout
    Former-commit-id: 63cec9c5b06c2bd0bc19319a56f1cbd0bc03b009

--- frontend/src/UI/pages/NonLoginMain.jsx
+++ frontend/src/UI/pages/NonLoginMain.jsx
@@ -2,11 +2,12 @@ import NeonLogo from "../atoms/NeonLogo";
 
 function NonLoginMain() {
   return (
-    // <Link to="/login">
-    <div className="flex h-full justify-center items-center">
+    // <div className="flex h-full justify-center items-center">
+    //   <NeonLogo />
+    // </div>
+    <div className="flex justify-center h-screen">
       <NeonLogo />
     </div>
-    // </Link>
   );
 }
 
