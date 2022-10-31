89dcc09a649b6f03572d73243caa29f15d2823cd
yoonsunny17 <jyoonsun0217@gmail.com>
2022-11-01 00:02:27 +0900

    [FE] fix: update form tag for profile
    Former-commit-id: fc97b99971bfc25b343ebbab00aac36cccb58b40

--- frontend/src/UI/organisms/LobbyProfile.jsx
+++ frontend/src/UI/organisms/LobbyProfile.jsx
@@ -1,179 +1,112 @@
 import { useState, useEffect } from "react";
 
 function LobbyProfile() {
-  const [userInfo, setUserInfo] = useState({
-    nickName: "Zl존윤sun",
-    message: "늦었다고 생각할때가 진짜 너무 늦었다",
-  });
-  const [isEdited, setIsEdited] = useState(true);
-  const [editNickName, setEditNickName] = useState("");
-  const [editMessage, setEditMessage] = useState("");
-  const [editNickNameValid, setEditNickNameValid] = useState({
-    isValid: true,
+  const initialUserInfo = {
+    nickname: "zl존윤sun",
+    message: "늦었다고 생각할 때가 진짜 너무 늦었다",
+  };
+
+  const [userInfo, setUserInfo] = useState(initialUserInfo);
+  console.log(userInfo);
+  // edit ? "편집하는 중!" : "편집 안하는 중"
+  const [edit, setEdit] = useState(false);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    setUserInfo((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  // 유효성 검사
+  const [isValid, setIsValid] = useState({
+    valid: true,
     message: "",
   });
-
-  useEffect(() => {
-    //axios 요청으로 유저정보 가져오기
-    //setUserInfo(response.data);
-    setEditNickName(userInfo.nickName);
-  }, []);
-
-  const editNickNameHandleChange = (e) => {
-    setEditNickName(e.target.value);
-  };
-  const editMessageHandleChange = (e) => {
-    setEditMessage(e.target.value);
-  };
-  const editNickNameBlurHandle = (e) => {
+  const handleBlur = (e) => {
     let nickNameRE = new RegExp("^[a-zA-Z0-9가-힣_]{2,12}$");
-    if (!nickNameRE.test(editNickName)) {
-      // TODO: 몇글자인지 정하기
-      setEditNickNameValid({
-        isValid: false,
-        message: "*2~12글자 사이로 입력해주세요.",
+    if (!nickNameRE.test(userInfo.nickname)) {
+      setIsValid({
+        valid: false,
+        message: "*2~12글자 사이로 입력해주세요",
       });
+      console.log(isValid.message);
       return;
     }
-    setEditNickNameValid({ isValid: true, message: "" });
+    setIsValid({ valid: true, message: "" });
     //axios 요청으로 닉네임 중복검사하기
     // setNickNameValid({ isValid: true, message: "이미 가입된 닉네임입니다." });
   };
 
-  const editOnOff = () => {
-    if (isEdited) {
-      setEditNickName(userInfo.nickName);
-      setEditMessage(userInfo.message);
-    } else {
-      if (editNickNameValid.isValid) {
-        return;
-      }
-      changeProfile();
-    }
-    setIsEdited(!isEdited);
-  };
-
-  const changeProfile = async () => {
-    //axios로 프로필 수정 요청 보내기
-    setUserInfo({
-      message: editMessage,
-      nickName: editNickName,
-      userImg: userInfo.userImg,
-    });
-  };
-
-  const handleEnter = (e) => e.key === "Enter" && e.preventDefault();
-
   return (
     <div className="flex flex-col items-center w-full mt-28 border border-1 border-white rounded-2xl bg-white">
       {/* header */}
-      <div className="flex justify-center items-center text-white w-full rounded-tl-2xl rounded-tr-2xl bg-gradient-to-t from-main1 to-sub1">
-        <p className="text-2xl drop-shadow font-bold my-4">프로필</p>
+      <div className="flex justify-center items-center w-full rounded-tl-2xl rounded-tr-2xl bg-gradient-to-t from-main1 to-sub1">
+        <p className="text-2xl drop-shadow font-bold my-4 text-white">프로필</p>
       </div>
+      {/* profile contents */}
       <form
-        onSubmit={changeProfile}
-        onKeyPress={handleEnter}
+        onSubmit={handleSubmit}
         className="flex flex-col w-full items-center"
       >
+        {/* nickname */}
         <div className="w-11/12 my-3">
-          {/* nickname */}
           <div>
             <input
-              disabled={isEdited}
+              disabled={!edit}
               type="text"
               placeholder="닉네임"
-              value={editNickName}
-              onChange={editNickNameHandleChange}
-              onBlur={editNickNameBlurHandle}
+              name="nickname"
+              value={userInfo.nickname}
+              onChange={handleChange}
+              onBlur={handleBlur}
               className={
-                `h-12 focus:outline-none rounded-lg w-full px-3` +
-                // editNickNameValid가 True이면 유효한거..
-                (editNickNameValid.isValid
-                  ? " border-2 border-base2/20"
-                  : " border-2 border-red-500/80")
+                `h-12 focus:outline-none rounded-lg w-full px-3 border-2` +
+                (isValid.valid ? " border-base2/20" : " border-red-500/80")
               }
             />
             <p className="text-red-500 ml-1 mt-1 text-[10px]">
-              {editNickNameValid.message}
+              {isValid.message}
             </p>
           </div>
           {/* comments */}
-          <div className="pt-2">
+          <div>
             <textarea
-              disabled={isEdited}
+              disabled={!edit}
+              name="message"
+              value={userInfo.message}
+              id="message"
+              rows="6"
               placeholder="상태 메세지를 입력하세요"
-              className="w-full py-2 focus:outline-none rounded-lg resize-none mb-4 px-2 border-2 border-base2/20"
-              rows={6}
-              name="comments"
-              id="comments"
-              onChange={editMessageHandleChange}
-              value={editMessage}
+              className="pt-2 w-full py-2 focus:outline-none rounded-lg resize-none mb-4 px-2 border-2 border-base2/20"
+              onChange={handleChange}
             ></textarea>
           </div>
           {/* submit button */}
-          <button className="font-bold text-lg drop-shadow text-white bg-gradient-to-t from-extra1 to-extra2 h-14 w-full rounded-2xl ">
-            {isEdited ? "편집 완료" : "편집 하기"}
-          </button>
+          {edit && (
+            <button
+              type="submit"
+              onClick={() => setEdit(false)}
+              className="font-bold text-lg drop-shadow text-white bg-gradient-to-t from-extra1 to-extra2 h-14 w-full rounded-2xl"
+            >
+              편집 완료
+            </button>
+          )}
+          {!edit && (
+            <button
+              onClick={() => setEdit(true)}
+              className="font-bold text-lg drop-shadow text-white bg-gradient-to-t from-extra1 to-extra2 h-14 w-full rounded-2xl"
+            >
+              프로필 편집
+            </button>
+          )}
         </div>
       </form>
-      {/* nickname input & comment input */}
-      {/* {!edit ? (
-        <div className="flex flex-col w-full items-center">
-          <div className="w-11/12 my-3">
-            <input
-              type="text"
-              placeholder="닉네임"
-              className="w-full py-4 focus:outline-none rounded-lg shadow mb-4 px-2"
-            />
-          </div>
-          <textarea
-            placeholder="상태 메세지를 입력하세요"
-            className="w-11/12 py-4 focus:outline-none rounded-lg resize-none shadow mb-4 px-2"
-            rows={6}
-            name="comments"
-            id="comments"
-          ></textarea>
-
-          <div className="mb-6 w-11/12 h-16 flex items-center justify-center border border-1 border-white rounded-3xl bg-gradient-to-t from-extra1 to-extra2">
-            <button
-              onClick={() => {
-                editOnOff();
-              }}
-              className="font-bold text-lg drop-shadow text-white "
-            >
-              편집 완료
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className="flex flex-col w-full items-center">
-          <div className="w-11/12 my-3">
-            <input
-              type="text"
-              placeholder="닉네임"
-              className="w-full py-4 focus:outline-none rounded-lg shadow mb-4 px-2"
-            />
-          </div>
-          <textarea
-            placeholder="상태 메세지를 입력하세요"
-            className="w-11/12 py-4 focus:outline-none rounded-lg resize-none shadow mb-4 px-2"
-            rows={6}
-            name="comments"
-            id="comments"
-          ></textarea>
-
-          <div className="mb-6 w-11/12 h-16 flex items-center justify-center border border-1 border-white rounded-3xl bg-gradient-to-t from-extra1 to-extra2">
-            <button
-              onClick={() => {
-                editOnOff();
-              }}
-              className="font-bold text-lg drop-shadow text-white "
-            >
-              편집 완료
-            </button>
-          </div>
-        </div>
-      )} */}
     </div>
   );
 }
