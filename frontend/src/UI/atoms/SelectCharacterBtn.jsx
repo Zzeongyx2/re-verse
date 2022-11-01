296e84a2051c4c3f61fb42671f0315bcd10e331c
yoonsun <jyoonsun0217@gmail.com>
2022-11-01 13:42:03 +0900

    [FE] feat: select character radio btn
    Former-commit-id: a78e1135c7ff8e194cec82f9f4a4c0b666ea8ad8

--- frontend/src/UI/atoms/SelectCharacterBtn.jsx
+++ frontend/src/UI/atoms/SelectCharacterBtn.jsx
@@ -1,12 +1,37 @@
+import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react";
+
+import { avatarAnatomy } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers, defineStyle } from "@chakra-ui/react";
+
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(avatarAnatomy.keys);
+
+const superLg = defineStyle({
+  width: 40,
+  height: 40,
+  fontSize: "6xl",
+});
+
+const sizes = {
+  superLg: definePartsStyle({ container: superLg }),
+};
+
+export const avatarTheme = defineMultiStyleConfig({ sizes });
+
 function SelectCharacterBtn({ imgUrl, name }) {
   return (
-    <img
-      src={imgUrl}
-      alt={name}
-      className="w-16 h-16 object-cover rounded-full p-1"
-    />
-    // <div>
-    // </div>
+    <>
+      <div>
+        <Avatar size="xl" alt={name} src={imgUrl} className="p-2" />
+      </div>
+    </>
+    // <button>
+    //   <img
+    //     src={imgUrl}
+    //     alt={name}
+    //     className="w-20 h-20 object-cover rounded-full p-1"
+    //   />
+    // </button>
   );
 }
 
