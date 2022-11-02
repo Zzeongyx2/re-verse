955b464cd930c5644c373472c93cbd41a1c016d7
parkwonchang <parkwc0213@naver.com>
2022-11-02 11:41:49 +0900

    [BE] feat: friend bidirectional
    Former-commit-id: 1511e24d211a8986ea5d1b8bb94db1a53b2a57d9

--- backend/archive/src/main/java/kr/co/reverse/archive/db/repository/FriendRepository.java
+++ backend/archive/src/main/java/kr/co/reverse/archive/db/repository/FriendRepository.java
@@ -3,6 +3,7 @@ package kr.co.reverse.archive.db.repository;
 import kr.co.reverse.archive.db.entity.Friend;
 import kr.co.reverse.archive.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
