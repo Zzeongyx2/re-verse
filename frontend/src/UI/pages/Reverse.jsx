5dc91e3d3dea7358c076a977ab260344e2b485ed
yoonsun <jyoonsun0217@gmail.com>
2022-11-03 17:36:11 +0900

    [FE] feat: controlling multiple character animations in react three fiber
    Former-commit-id: 82c177198180e4d06f969d11f530fa26b39ff705

--- frontend/src/UI/pages/Reverse.jsx
+++ frontend/src/UI/pages/Reverse.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
 import CatAnimations from "../../assets/animals/Cat_Animations.js";
@@ -7,15 +7,53 @@ import { OrbitControls } from "@react-three/drei/core/OrbitControls.js";
 // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function Reverse() {
+  const [action, setAction] = useState("Idle_A");
   return (
     <div className="h-screen">
+      <div className=" mt-4">
+        <button
+          className="bg-white mx-2 text-lg px-4"
+          onClick={() => {
+            setAction("Idle_A");
+          }}
+        >
+          idle
+        </button>
+        <button
+          className="bg-white mx-2 text-lg px-4"
+          onClick={() => {
+            setAction("Roll");
+          }}
+        >
+          roll
+        </button>
+        <button
+          className="bg-white mx-2 text-lg px-4"
+          onClick={() => {
+            setAction("Jump");
+          }}
+        >
+          jump
+        </button>
+        <button
+          className="bg-white mx-2 text-lg px-4"
+          onClick={() => {
+            setAction("Walk");
+          }}
+        >
+          walk
+        </button>
+      </div>
       <Canvas>
         <OrbitControls />
         <directionalLight intensity={0.3} />
         <ambientLight intensity={0.2} />
-        {/* <Suspense fallback={null}> */}
-        <CatAnimations />
-        {/* </Suspense> */}
+        <pointLight intensity={2} position={[-1, 1, 3]} color="red" />
+        <pointLight intensity={2} position={[1, 1, 3]} color="blue" />
+        <pointLight intensity={2} position={[0, 3, -10]} color="white" />
+        <Suspense fallback={null}>
+          <CatAnimations action={action} />
+        </Suspense>
       </Canvas>
     </div>
   );
