7871ecbf699a471ff23dab7203168ecd36aef256
지용현 <yung5487@gmail.com>
2022-11-04 14:17:43 +0900

    [FE] feat: create animal character gltf to js file
    Former-commit-id: bc24dbdb0e3da17186a11b860d067f64662a4d47

---
+++ frontend/src/assets/characters/DoveCharacter.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+
+export default function DoveCharacter() {
+  const group = useRef();
+  const [action, setAction] = useState("Idle_A");
+
+  const previousAction = usePrevious(action);
+  const { nodes, materials, animations } = useGLTF(
+    "/assets/animals/GLTF/Animations/Dove_Animations.gltf"
+  );
+  const { actions } = useAnimations(animations, group);
+  const characterClick = () => {
+    if (action === "Idle_A") {
+      setAction("Roll");
+    } else {
+      setAction("Idle_A");
+    }
+  };
+  useEffect(() => {
+    if (previousAction) {
+      actions[previousAction].fadeOut(0.2);
+      actions[action].stop();
+    }
+    actions[action].play();
+    actions[action].fadeIn(0.2);
+  }, [actions, action, previousAction]);
+  return (
+    <group ref={group} dispose={null}>
+      <group name="Scene">
+        <group name="Rig">
+          <primitive object={nodes.root} />
+          <skinnedMesh
+            name="Dove"
+            geometry={nodes.Dove.geometry}
+            material={materials.M_Dove}
+            skeleton={nodes.Dove.skeleton}
+            morphTargetDictionary={nodes.Dove.morphTargetDictionary}
+            morphTargetInfluences={nodes.Dove.morphTargetInfluences}
+            // 그림자 설정은 여기에!
+            castShadow
+            receiveShadow
+            onClick={() => {
+              characterClick();
+            }}
+          />
+        </group>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/assets/animals/GLTF/Animations/Dove_Animations.gltf");
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
+}
