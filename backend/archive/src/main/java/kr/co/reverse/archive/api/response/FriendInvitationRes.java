9ed9275c6414867ad345f436ef8d9850a6b37e79
parkwonchang <parkwc0213@naver.com>
2022-11-08 17:08:37 +0900

    [BE] feat: friend waitingTo, From finished
    Former-commit-id: f5f15e91258acc1867a4f75c8eb32f331c532316

--- backend/archive/src/main/java/kr/co/reverse/archive/api/response/FriendInvitationRes.java
+++ backend/archive/src/main/java/kr/co/reverse/archive/api/response/FriendInvitationRes.java
@@ -1,10 +1,16 @@
 package kr.co.reverse.archive.api.response;
 
+import com.querydsl.core.annotations.QueryProjection;
 import kr.co.reverse.archive.db.entity.FriendInvitation;
 import kr.co.reverse.archive.db.entity.User;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 public class FriendInvitationRes {
 
     private String nickname;
@@ -12,6 +18,15 @@ public class FriendInvitationRes {
     private String avatar;
 
     private String message;
+
+    @QueryProjection
+    @Builder
+    public FriendInvitationRes(String nickname, String avatar, String message){
+        this.nickname = nickname;
+        this.avatar = avatar;
+        this.message = message;
+
+    }
 
     public static FriendInvitationRes of(User user) {
         if (user == null) return null;
