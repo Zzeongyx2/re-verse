93b7094d057768375dbf8efd71549cd2f7a25d6c
giyeon3145 <giyeon3145@gmail.com>
2022-11-08 13:51:16 +0900

    [FE] feat: multiple connection
    Former-commit-id: 66e6ef2ec1f4fa83cce4068c3bc9ee74e8028203

--- frontend/src/UI/organisms/ReverseFooter.jsx
+++ frontend/src/UI/organisms/ReverseFooter.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-
+import ReverseWebRTC from "../pages/ReverseWebRTC";
 // TODO: 기연아 채팅창 여기다 만들면 돼!!!
 // TODO: 기연아 채팅창 여기다 만들면 돼!!!
 // TODO: 기연아 채팅창 여기다 만들면 돼!!!
@@ -7,9 +7,12 @@ import { Link, useNavigate } from "react-router-dom";
 // TODO: 기연아 채팅창 여기다 만들면 돼!!!
 
 function ReverseFooter() {
-  const navigate = useNavigate();
-
-  return <div className="mx-4 border-2 w-full h-full">this is footer!!</div>;
+  
+  return (
+    <div className="mx-4 border-2 w-full h-full bg-white">
+        
+    </div>
+  );
 }
 
 export default ReverseFooter;
