cc6b14251d899c1e599e5bdc29019514786ba231
parkwonchang <parkwc0213@naver.com>
2022-11-09 11:37:14 +0900

    [BE] feat: get archiveMemberList
    Former-commit-id: 5ab90d6ed571b8df0f9faa8a8687fc315018ece0

--- backend/archive/src/main/java/kr/co/reverse/archive/db/repository/ArchiveMemberRepositoryImpl.java
+++ backend/archive/src/main/java/kr/co/reverse/archive/db/repository/ArchiveMemberRepositoryImpl.java
@@ -2,6 +2,8 @@ package kr.co.reverse.archive.db.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.co.reverse.archive.db.entity.ArchiveMember;
+import kr.co.reverse.archive.db.entity.QArchive;
+import kr.co.reverse.archive.db.entity.QArchiveMember;
 import kr.co.reverse.archive.db.entity.User;
 
 import javax.persistence.EntityManager;
@@ -17,6 +19,15 @@ public class ArchiveMemberRepositoryImpl implements ArchiveMemberRepositoryCusto
 
     @Override
     public List<ArchiveMember> archiveMemberList(User user, User target) {
-        return null;
+        return jpaQueryFactory
+                .select(
+                        QArchiveMember.archiveMember
+                )
+                .from(QArchiveMember.archiveMember)
+                .join(QArchiveMember.archiveMember.archive, QArchive.archive)
+                .where(
+                        QArchiveMember.archiveMember.archive.ownerId.eq(user.getId())
+                                .and(QArchiveMember.archiveMember.user.eq(target))
+                ).fetch();
     }
 }
