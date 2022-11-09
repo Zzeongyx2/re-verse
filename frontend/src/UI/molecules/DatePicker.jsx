1c2c4edc5ea21e42821a4c6680efbf883ba8060a
yoonsun <jyoonsun0217@gmail.com>
2022-11-09 13:41:18 +0900

    [FE] feat: react datepicker
    Former-commit-id: 7336b683ab42297ddf5c472dde9cdbf8838d6c00

---
+++ frontend/src/UI/molecules/DatePicker.jsx
@@ -0,0 +1,11 @@
+import DatePicker from "react-datepicker";
+
+function ArchiveDatePicker() {
+  return (
+    <div>
+      <div>this is date picker!</div>
+    </div>
+  );
+}
+
+export default ArchiveDatePicker;
