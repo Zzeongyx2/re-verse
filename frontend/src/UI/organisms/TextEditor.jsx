27c3fbd0f9e5dbbc3f161a56637c40237c12b97b
yoonsun <jyoonsun0217@gmail.com>
2022-11-10 14:22:08 +0900

    [FE] feat: apply html converter
    Former-commit-id: 79c79df84cb7aef5302a3ede1cdb8cd879903839

--- frontend/src/UI/organisms/TextEditor.jsx
+++ frontend/src/UI/organisms/TextEditor.jsx
@@ -1,12 +1,26 @@
 import React, { Component, useState } from "react";
-import { EditorState } from "draft-js";
+import { convertFromRaw, convertToRaw, EditorState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import "../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "../../styles/draft.css";
+import { convertToHTML, convertFromHTML } from "draft-convert";
+import draftToHtml from "draftjs-to-html";
+import htmlToDraft from "html-to-draftjs";
+
+// import { SketchPicker } from "react-color";
+// import ColorPic from "../molecules/ColorPicker";
 
 // export const EditorComponent = () => (
 //   // <Editor />
 //   <Editor toolbarClassName="" />
 // );
+
+function uploadImageCallBack(file) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://");
+  });
+}
 
 export const EditorComponent = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
@@ -15,29 +29,58 @@ export const EditorComponent = () => {
     setEditorState(editorState);
   };
 
+  const [exportContent, setExportContent] = useState();
+  const exportHTML = () => {
+    setExportContent({
+      convertedContent: convertToHTML(editorState.getCurrentContent()),
+    });
+  };
+
+  console.log(exportContent);
+
+  // const [updateContent, setUpdateContent] = useState();
+  // const updateHTML = (e) => {
+  //   e.preventDefault();
+  //   setUpdateContent({ convertedContent: e.target.value });
+  // };
+
+
+
   return (
-    <Editor
-      // 에디터 & 툴바 모두 적용되는 클래스
-      wrapperClassName="wrapper-class"
-      // 에디터 주변에 적용되는 클래스
-      editorClassName="editor"
-      // 툴바 주위에 적용되는 클래스
-      toolbarClassName="toolbar-class"
-      // 툴바 설정
-      toolbar={{
-        // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
-        list: { inDropdown: true },
-        // textAlign: { inDropdown: true },
-      }}
-      placeholder="내용을 작성해주세요"
-      // 한국어 설정... 이거 디폴트 값 설정하는건가? 잘 모르겠음
-      localization={{
-        locale: "ko",
-      }}
-      // 초기값 설정
-      editorState={editorState}
-      // 에디터의 값이 변경될 때마다 호출하는 함수
-      onEditorStateChange={onEditorStateChange}
-    />
+    <div>
+      <Editor
+        // 에디터 & 툴바 모두 적용되는 클래스
+        wrapperClassName="wrapper"
+        // wrapperClassName="wrapper-class"
+        // 에디터 주변에 적용되는 클래스
+        editorClassName="editor"
+        // 툴바 주위에 적용되는 클래스
+        toolbarClassName="toolbar"
+        // 툴바 설정
+        toolbar={{
+          // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
+          // list: { inDropdown: true },
+          link: { inDropdown: true },
+          history: { inDropdown: true },
+          image: {
+            uploadCallback: uploadImageCallBack,
+            alt: { present: true, mandatory: true },
+          },
+          textAlign: { className: "textalign" },
+          // colorPicker: { component: ColorPic },
+        }}
+        // placeholder="내용을 작성해주세요"
+        localization={{
+          locale: "ko",
+        }}
+        // 초기값 설정
+        editorState={editorState}
+        // 에디터의 값이 변경될 때마다 호출하는 함수
+        onEditorStateChange={onEditorStateChange}
+      />
+      <button onClick={exportHTML}>convert to HTML</button>
+      {/* importHTML */}
+      <input type="text" />
+    </div>
   );
 };
