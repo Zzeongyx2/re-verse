91045aa863d1061044605b62899a6aa9092f77e0
yoonsun <jyoonsun0217@gmail.com>
2022-11-11 14:20:54 +0900

    [FE] fix: save article title, memory date
    Former-commit-id: 42479f407091d02900da337fd4f0f39ff05f05bf

--- frontend/src/UI/molecules/ReverseDatePicker.jsx
+++ frontend/src/UI/molecules/ReverseDatePicker.jsx
@@ -1,21 +1,36 @@
+import { useEffect } from "react";
 import { useRef } from "react";
 import { forwardRef, useState } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+import { useDispatch, useSelector } from "react-redux";
+import { createArticle } from "../../modules/reverse";
 
 function ReverseDatePicker() {
-  const [startDate, setStartDate] = useState(new Date());
+  const dispatch = useDispatch();
+  const reverse = useSelector((state) => state.reverse);
+
+  const today = new Date();
+  const [startDate, setStartDate] = useState(today);
   const InputBtn = forwardRef(({ value, onClick }, ref) => (
     <button className="mt-0.5 font-semibold" onClick={onClick} ref={ref}>
       {value}
     </button>
   ));
+
+  useEffect(() => {
+    console.log(reverse);
+  }, [reverse]);
   return (
     <DatePicker
       dateFormat="yyyy.MM.dd (eee)"
       selected={startDate}
-      onChange={(date) => setStartDate(date)}
+      // onChange={(date) => setStartDate(date)}
+      onChange={(date) => {
+        setStartDate(date);
+        dispatch(createArticle({ ...reverse.article, memoryDate: `${date}` }));
+      }}
       customInput={<InputBtn />}
       maxDate={new Date()}
     />
