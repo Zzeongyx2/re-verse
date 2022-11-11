a8410062914530f305d5c0cfa944751b15a8b2ce
parkwonchang <parkwc0213@naver.com>
2022-11-11 14:32:32 +0900

    [BE] fix: execption "CHO HYE EUN" to "Not Friend"22
    Former-commit-id: 7bb51f95b1ce1a24755a660187ae0ba71b13d02f

--- backend/archive/src/main/java/kr/co/reverse/archive/common/exception/AlreadyFriendException.java
+++ backend/archive/src/main/java/kr/co/reverse/archive/common/exception/AlreadyFriendException.java
@@ -1,12 +1,11 @@
 package kr.co.reverse.archive.common.exception;
 
-
+import kr.co.reverse.archive.common.error.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class NotFriendException extends RuntimeException{
+public class AlreadyFriendException extends RuntimeException{
 
-//    private final ErrorCode errorCode;
 }
