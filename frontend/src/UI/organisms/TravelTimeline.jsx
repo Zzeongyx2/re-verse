38669d9af15c7d4e5f91f930d65ec3cf79daeade
yoonsunny17 <jyoonsun0217@gmail.com>
2022-11-14 01:31:38 +0900

    [FE] fix: update timeline - still working..
    Former-commit-id: 5624957f377e7d4a4d5df0b9e042eb0f2dea3a37

--- frontend/src/UI/organisms/TravelTimeline.jsx
+++ frontend/src/UI/organisms/TravelTimeline.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getStuffDetail } from "../../api/reverse";
-import { setTravel } from "../../modules/reverse";
+import { getPaper, getStuffDetail } from "../../api/reverse";
+import { setInfo, setTravel } from "../../modules/reverse";
 
 import { BsDot } from "react-icons/bs";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
@@ -10,29 +10,31 @@ function ArticlesTimeline() {
   const dispatch = useDispatch();
   const reverse = useSelector((state) => state.reverse);
 
-  // useEffect(() => {
-  //   getStuffDetail(
-  //     reverse.info.archiveId,
-  //     reverse.info.stuffs[0].id,
-  //     getDetailSuccess,
-  //     getDetailFail
-  //   );
-  // }, []);
-  // const getDetailSuccess = (res) => {
-  //   console.log(res);
-  //   dispatch(setTravel({ ...reverse.travel, articleList: res.data.papers }));
-  //   // dispatch(setTravelArticleList(res.data.papers));
-  // };
-  // const getDetailFail = (err) => {
-  //   console.log(err);
-  // };
-
   // memory time 순서대로 정렬
   [...reverse.travel.articleList].sort(
     (a, b) => new Date(a.memoryTime) - new Date(b.memoryTime)
   );
 
   const weekDay = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  const handleGetDetail = async (paperId) => {
+    await getPaper(
+      reverse.info.archiveId,
+      reverse.info.stuffs[0].id,
+      paperId,
+      success,
+      fail
+    );
+  };
+
+  const success = (res) => {
+    console.log(res);
+    dispatch(setInfo({ ...reverse.info, details: res.data }));
+  };
+
+  const fail = (err) => {
+    console.log(err);
+  };
 
   return (
     <div className="w-5/6 mx-auto relative">
@@ -60,12 +62,18 @@ function ArticlesTimeline() {
       {reverse.travel.articleList.length &&
         reverse.travel.articleList.map((article, idx) => {
           return (
-            <div key={`travel-${idx}`} className="mb-5">
+            <div
+              key={`travel-${idx}`}
+              onClick={() => {
+                handleGetDetail(article.id);
+              }}
+              className="mb-5 cursor-pointer border-2 px-2 py-2 rounded-md"
+            >
               <div className="font-semibold text-base1">
                 {new Date(article.memoryTime).toLocaleDateString() + " "}
                 {weekDay[new Date(article.memoryTime).getDay()]}
               </div>
-              <div>{article.title}</div>
+              <div className="text-ellipsis line-clamp-1">{article.title}</div>
             </div>
           );
         })}
