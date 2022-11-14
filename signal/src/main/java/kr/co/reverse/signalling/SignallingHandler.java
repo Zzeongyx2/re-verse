3ad8a3fbe1251c4a8d638c033e574a3a2bc1f2b6
giyeon3145 <giyeon3145@gmail.com>
2022-11-14 16:44:14 +0900

    [BE] fix: signalling server
    Former-commit-id: db605c41e9bc3807bf6b669d3b3660f1301f34b6

--- signal/src/main/java/kr/co/reverse/signalling/SignallingHandler.java
+++ signal/src/main/java/kr/co/reverse/signalling/SignallingHandler.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import kr.co.reverse.model.WebSocketDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -18,37 +19,42 @@ import kr.co.reverse.model.SignalType;
 
 public class SignallingHandler extends TextWebSocketHandler {
 
-    Map<String, WebSocketDto> archiveMap = new HashMap<>();
-    String archive="";
+
+    Set<WebSocketSession> sessions = new HashSet<WebSocketSession>();
+    Map<String, ConcurrentHashMap<String, WebSocketSession>> archiveMap=new HashMap<>();
     final ObjectMapper map1 = new ObjectMapper();
     Logger log1 = LoggerFactory.getLogger(SignallingHandler.class);
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println("is packet arrived?");
+        System.out.println("this archive's size : "+archiveMap.size());
         final String msg1 = message.getPayload();
-        System.out.println(msg1);
         SignalData sigData = map1.readValue(msg1, SignalData.class);
-        archive = sigData.getArchive();
         log1.debug("Receive message from client:", msg1);
-        if (!archiveMap.containsKey(archive)) {
-            archiveMap.put(archive, new WebSocketDto(new LinkedList<>(),new ConcurrentHashMap<>()));
+        String archive= sigData.getArchivId();
+        if(!archiveMap.containsKey(archive)){
+            archiveMap.put(archive, new ConcurrentHashMap<String, WebSocketSession>());
         }
-        List<WebSocketSession> sessions = archiveMap.get(archive).getSession();
-        ConcurrentHashMap<String, WebSocketSession> sessionMap=archiveMap.get(archive).getSessionMap();
+        ConcurrentHashMap<String, WebSocketSession> sessionMap=archiveMap.get(archive);
+
         SignalData sigResp = new SignalData();
         if (sigData.getType().equalsIgnoreCase(SignalType.Login.toString())) {
+            System.out.println("\there is login phase");
+
             SignalData sigResp2 = new SignalData();
             String userId = UUID.randomUUID().toString();
             sigResp2.setUserId("signaling");
             sigResp2.setType(SignalType.UserId.toString());
             sigResp2.setData(userId);
+            synchronized (sessionMap) {
+                session.getAttributes().put("archive", archive);
+                session.getAttributes().put("uuid", userId);
+            }
             sessionMap.put(userId, session);
             session.sendMessage(new TextMessage(map1.writeValueAsString(sigResp2)));
-            return;
         } else if (sigData.getType().equalsIgnoreCase(SignalType.NewMember.toString())) {
+            System.out.println("\there is new member phase");
             sessionMap.values().forEach(a -> {
-                System.out.println(a.toString());
                 SignalData sigResp2 = new SignalData();
                 sigResp2.setUserId(sigData.getUserId());
                 sigResp2.setType(SignalType.NewMember.toString());
@@ -62,7 +68,9 @@ public class SignallingHandler extends TextWebSocketHandler {
                     log1.error("Error Sending message:", e);
                 }
             });
+
         } else if (sigData.getType().equalsIgnoreCase(SignalType.Offer.toString())) {
+            System.out.println("\there is offer phase");
             sigResp = new SignalData();
             sigResp.setUserId(sigData.getUserId());
             sigResp.setType(SignalType.Offer.toString());
@@ -71,7 +79,9 @@ public class SignallingHandler extends TextWebSocketHandler {
             synchronized (sessionMap) {
                 sessionMap.get(sigData.getToUid()).sendMessage(new TextMessage(map1.writeValueAsString(sigResp)));
             }
+
         } else if (sigData.getType().equalsIgnoreCase(SignalType.Answer.toString())) {
+            System.out.println("\there is answer phase");
             sigResp = new SignalData();
             sigResp.setUserId(sigData.getUserId());
             sigResp.setType(SignalType.Answer.toString());
@@ -80,7 +90,9 @@ public class SignallingHandler extends TextWebSocketHandler {
             synchronized (sessionMap) {
                 sessionMap.get(sigData.getToUid()).sendMessage(new TextMessage(map1.writeValueAsString(sigResp)));
             }
+
         } else if (sigData.getType().equalsIgnoreCase(SignalType.Ice.toString())) {
+            System.out.println("\there is ice phase");
             sigResp = new SignalData();
             sigResp.setUserId(sigData.getUserId());
             sigResp.setType(SignalType.Ice.toString());
@@ -94,15 +106,16 @@ public class SignallingHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        archiveMap.get(archive).getSession().add(session);
-        System.out.println("after add : " + archiveMap.get(archive).getSession().toString());
+        System.out.println("after connection established");
+        sessions.add(session);
         super.afterConnectionEstablished(session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        archiveMap.get(archive).getSession().remove(session);
-        System.out.println("after remove : " + archiveMap.get(archive).getSession().toString());
+        System.out.println("after connection closed");
+        archiveMap.get(session.getAttributes().get("archive")).remove(session.getAttributes().get("uuid"));
+        sessions.remove(session);
         super.afterConnectionClosed(session, status);
     }
 }
