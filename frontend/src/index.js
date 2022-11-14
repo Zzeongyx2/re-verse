41d154a0f2e26879c1e1698d902585f58b731427
지용현 <yung5487@gmail.com>
2022-11-14 17:09:32 +0900

    [FE] fix: apply reverse lazy func
    Former-commit-id: 3c0518d3eb250fca4b69ce9a996bbc18fb2b5b03

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,7 +23,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    <PersistGate loading={<div>로딩중!!</div>} persistor={persistor}>
       <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
