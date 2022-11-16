adfcaff065e84c37aa7642bee153f3f6e2bdfd92
정기연 <giyeon3145@gmail.com>
2022-11-16 13:01:43 +0900

    [FE] feat: cannon demo version
    Former-commit-id: a0855d2f2c21e710afefb30babf7cd89bbd06b3c

--- frontend/src/UI/atoms/SelectedMyPlayer.jsx
+++ frontend/src/UI/atoms/SelectedMyPlayer.jsx
@@ -1,78 +1,79 @@
 import CatAnimations from "../../assets/players/Cat_Animations";
 import { useSelector, useDispatch } from "react-redux";
-import { useBox, useCylinder } from "@react-three/cannon";
+import { useBox, useCylinder, useCompoundBody } from "@react-three/cannon";
 import { useFrame } from "@react-three/fiber";
 import { Vector3 } from "three";
-
+import userEvent from "@testing-library/user-event";
+import { useRef } from "react";
+import { Box } from "./Collider.jsx";
 function SelectedMyPlayer({ destinationPoint, handleVisible }) {
   const loginUser = useSelector((state) => state.user.loginUser);
-  const [playerCharacter] = useBox(() => ({
-    mass: 1,
-    args: [2, 1, 2],
-    position: [destinationPoint.x, 0, destinationPoint.z],
-  }));
+  const playerCharacter = useRef();
 
-  // useFrame(() => {
-  //   console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-  //   console.log(playerCharacter.current.position);
-  // });
+  useFrame(() => {
+    // console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+    // console.log(destinationPoint);
+  });
   return (
     <group ref={playerCharacter} dispose={null}>
-      {/* <group> */}
-      {loginUser.avatar === "Cat" ? (
-        <CatAnimations
-          destinationPoint={destinationPoint}
-          handleVisible={handleVisible}
-        />
-      ) : null}
-      {loginUser.avatar === "Dog" ? (
-        <CatAnimations
-          destinationPoint={destinationPoint}
-          handleVisible={handleVisible}
-        />
-      ) : null}
-      {loginUser.avatar === "Dove" ? (
-        <CatAnimations
-          destinationPoint={destinationPoint}
-          handleVisible={handleVisible}
-        />
-      ) : null}
-      {loginUser.avatar === "Goldfish" ? (
-        <CatAnimations
-          destinationPoint={destinationPoint}
-          handleVisible={handleVisible}
-        />
-      ) : null}
-      {loginUser.avatar === "Mouse" ? (
-        <CatAnimations
-          destinationPoint={destinationPoint}
-          handleVisible={handleVisible}
-        />
-      ) : null}
-      {loginUser.avatar === "Parrot" ? (
-        <CatAnimations
-          destinationPoint={destinationPoint}
-          handleVisible={handleVisible}
-        />
-      ) : null}
-      {loginUser.avatar === "Pigeon" ? (
-        <CatAnimations
-          destinationPoint={destinationPoint}
-          handleVisible={handleVisible}
-        />
-      ) : null}
-      {loginUser.avatar === "Rabbit" ? (
-        <CatAnimations
-          destinationPoint={destinationPoint}
-          handleVisible={handleVisible}
-        />
-      ) : null}
-      {loginUser.avatar === "Tortoise" ? (
-        <CatAnimations
-          destinationPoint={destinationPoint}
-          handleVisible={handleVisible}
-        />
-      ) : null}
+      {/* <Box destinationPoint={destinationPoint} /> */}
+      <Box destinationPoint={new Vector3(5, 2, 5)} />
+      <group>
+        {loginUser.avatar === "Cat" ? (
+          <CatAnimations
+            destinationPoint={destinationPoint}
+            handleVisible={handleVisible}
+          />
+        ) : null}
+        {loginUser.avatar === "Dog" ? (
+          <CatAnimations
+            destinationPoint={destinationPoint}
+            handleVisible={handleVisible}
+          />
+        ) : null}
+        {loginUser.avatar === "Dove" ? (
+          <CatAnimations
+            destinationPoint={destinationPoint}
+            handleVisible={handleVisible}
+          />
+        ) : null}
+        {loginUser.avatar === "Goldfish" ? (
+          <CatAnimations
+            destinationPoint={destinationPoint}
+            handleVisible={handleVisible}
+          />
+        ) : null}
+        {loginUser.avatar === "Mouse" ? (
+          <CatAnimations
+            destinationPoint={destinationPoint}
+            handleVisible={handleVisible}
+          />
+        ) : null}
+        {loginUser.avatar === "Parrot" ? (
+          <CatAnimations
+            destinationPoint={destinationPoint}
+            handleVisible={handleVisible}
+          />
+        ) : null}
+        {loginUser.avatar === "Pigeon" ? (
+          <CatAnimations
+            destinationPoint={destinationPoint}
+            handleVisible={handleVisible}
+          />
+        ) : null}
+        {loginUser.avatar === "Rabbit" ? (
+          <CatAnimations
+            destinationPoint={destinationPoint}
+            handleVisible={handleVisible}
+          />
+        ) : null}
+        {loginUser.avatar === "Tortoise" ? (
+          <CatAnimations
+            destinationPoint={destinationPoint}
+            handleVisible={handleVisible}
+          />
+        ) : null}
+      </group>
     </group>
   );
 }
