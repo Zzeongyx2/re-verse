62e73898b53bb903d4411ac9dac1d0757eed38a3
yoonsunny17 <jyoonsun0217@gmail.com>
2022-11-16 16:26:38 +0900

    [FE] test: positional audio - success!
    Former-commit-id: 72c195bcb823bde2be7b283b27e23f45a4396d07

--- frontend/src/UI/pages/PositionalAudio.jsx
+++ frontend/src/UI/pages/PositionalAudio.jsx
@@ -41,6 +41,7 @@ import { ForestKit } from "../../assets/deco/ForestKit.js";
 import { ReverseFloor } from "../../assets/deco/ReverseFloor.js";
 import { CampingMod } from "../../assets/deco/CampMod.js";
 import MusicTest from "../../assets/deco/MusicTest.js";
+import { Radio } from "../../assets/deco/Radio.js";
 
 var channels = [];
 var channelUsers = new Map();
@@ -51,7 +52,7 @@ var ws1;
 let localStream;
 let audioMapIdx = 0;
 
-function Reverse() {
+function PositionalAudio() {
   // default action = idle
   // const [characterPosition, setCharacterPosition] = useState();
   const [destinationPoint, setDestinationPoint] = useState(
@@ -202,19 +203,19 @@ function Reverse() {
     }
   }, [webrtcRedux.headCheck]);
 
-  useEffect(() => {
-    const bgmChristmas = document.getElementById("bgm-christmas");
-    const bgmTravel = document.getElementById("bgm-travel");
-    const bgmDiary = document.getElementById("bgm-diary");
-    const bgm = bgmDiary;
-    console.log(bgmChristmas);
-    bgm.volume = 0.1;
-    if (webrtcRedux.bgmCheck) {
-      bgm.muted = false;
-    } else {
-      bgm.muted = true;
-    }
-  }, [webrtcRedux.bgmCheck]);
+  // useEffect(() => {
+  //   const bgmChristmas = document.getElementById("bgm-christmas");
+  //   const bgmTravel = document.getElementById("bgm-travel");
+  //   const bgmDiary = document.getElementById("bgm-diary");
+  //   const bgm = bgmDiary;
+  //   console.log(bgmChristmas);
+  //   bgm.volume = 0.1;
+  //   if (webrtcRedux.bgmCheck) {
+  //     bgm.muted = false;
+  //   } else {
+  //     bgm.muted = true;
+  //   }
+  // }, [webrtcRedux.bgmCheck]);
   // ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
   async function login() {
     await navigator.mediaDevices
@@ -796,7 +797,7 @@ function Reverse() {
           enableZoom={true}
           // enableRotate={false}
           // minZoom={8.5}
-          maxZoom={20}
+          maxZoom={35}
         />
         {/* camera */}
         {/* perspective; 원근감 o, ortho; 원근감 x */}
@@ -869,7 +870,8 @@ function Reverse() {
           <Park />
 
           {/* music - positional audio */}
-          {/* <MusicTest /> */}
+          <MusicTest />
+          {/* <Radio /> */}
 
           {/* floor */}
           <ReverseFloor />
@@ -940,4 +942,4 @@ function Reverse() {
   );
 }
 
-export default Reverse;
+export default PositionalAudio;
---
+++ frontend/src/assets/deco/MusicTest.js
@@ -0,0 +1,91 @@
+import { PositionalAudio } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useEffect, useRef, useState } from "react";
+import * as THREE from "three";
+import { AudioListener, Camera, MeshPhongMaterial, PlaneGeometry } from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { PositionalAudioHelper } from "three/addons/helpers/PositionalAudioHelper.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { useSelector } from "react-redux";
+
+export default function MusicTest(params) {
+  const audioref = useRef();
+  const webrtcRedux = useSelector((state) => state.webrtc);
+
+  useEffect(() => {
+    if (webrtcRedux.bgmCheck) {
+      console.log("bgm on");
+    } else {
+      console.log("bgm off");
+    }
+  }, [webrtcRedux.bgmCheck]);
+
+  return (
+    <group>
+      <mesh
+        rotation={[-0.5 * Math.PI, 0, 0]}
+        position={[0, 1, 0]}
+        receiveShadow
+      >
+        <planeGeometry args={[10, 10]} />
+        <meshPhongMaterial color={0x999999} />
+      </mesh>
+      <gridHelper args={[10, 10]} position={[0, 1.01, 0]} />
+      <group>
+        {webrtcRedux.bgmCheck && webrtcRedux.musicTheme === 0 ? (
+          <PositionalAudio
+            ref={audioref}
+            autoplay
+            loop
+            url="https://re-verse-bucket.s3.ap-northeast-2.amazonaws.com/bgm/A-Picnic-With-My-Family_AdobeStock_452603097_preview.m4a"
+            distance={2}
+          />
+        ) : null}
+        {webrtcRedux.bgmCheck && webrtcRedux.musicTheme === 1 ? (
+          <PositionalAudio
+            ref={audioref}
+            autoplay
+            loop
+            url="https://re-verse-bucket.s3.ap-northeast-2.amazonaws.com/bgm/We-Wish-You-A-Merry-Christmas_AdobeStock_353746933_preview.m4a"
+            distance={2}
+          />
+        ) : null}
+        {webrtcRedux.bgmCheck && webrtcRedux.musicTheme === 2 ? (
+          <PositionalAudio
+            ref={audioref}
+            autoplay
+            loop
+            url="https://re-verse-bucket.s3.ap-northeast-2.amazonaws.com/bgm/Sweet-Mood_AdobeStock_452607713_preview.m4a"
+            distance={2}
+          />
+        ) : null}
+        {webrtcRedux.bgmCheck && webrtcRedux.musicTheme === 3 ? (
+          <PositionalAudio
+            ref={audioref}
+            autoplay
+            loop
+            url="https://re-verse-bucket.s3.ap-northeast-2.amazonaws.com/bgm/Emotional-Cinematic-Music-Box_AdobeStock_528505763_preview.m4a"
+            distance={2}
+          />
+        ) : null}
+        {webrtcRedux.bgmCheck && webrtcRedux.musicTheme === 4 ? (
+          <PositionalAudio
+            ref={audioref}
+            autoplay
+            loop
+            url="https://re-verse-bucket.s3.ap-northeast-2.amazonaws.com/bgm/TheCats.m4a"
+            distance={2}
+          />
+        ) : null}
+        {/* <PositionalAudio
+          ref={audioref}
+          autoplay
+          loop
+          url="https://re-verse-bucket.s3.ap-northeast-2.amazonaws.com/bgm/A-Picnic-With-My-Family_AdobeStock_452603097_preview.m4a"
+          distance={2}
+          onEnded
+        /> */}
+      </group>
+    </group>
+  );
+}
