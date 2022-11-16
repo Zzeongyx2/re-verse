bf4425aa8cd7bb20ce9580485c2696b2a0f4d393
yoonsunny17 <jyoonsun0217@gmail.com>
2022-11-16 23:12:25 +0900

    [FE] test: react three drei HTML effect
    Former-commit-id: dcb991db57dd703366eb9683785c265ee3429857

---
+++ frontend/src/UI/molecules/TestPage.jsx
@@ -0,0 +1,10 @@
+export default function TestPage() {
+  return (
+    <div className="w-full bg-white">
+      <div>hello?</div>
+      <div>hello?</div>
+      <div>hello?</div>
+      <div>hello?</div>
+    </div>
+  );
+}
