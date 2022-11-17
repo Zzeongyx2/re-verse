28fc4d2a87d4be7d2d0b2ef4e00fdf93d4d103ba
yoonsunny17 <jyoonsun0217@gmail.com>
2022-11-17 21:27:50 +0900

    [FE] fix: more bigger, more faster - perfect
    Former-commit-id: 0073f1a5c337868b7433633f7d8bc4cf18025e44

--- frontend/src/assets/players/my/MyDoveAnimations.js
+++ frontend/src/assets/players/my/MyDoveAnimations.js
@@ -27,7 +27,7 @@ export default function MyDoveAnimations({
   // }));
   // const previousAction = usePrevious(action);
   const { scene, materials, animations } = useGLTF(
-    "/assets/animals/GLTF/Animations/Dove_Animations.gltf",
+    "/assets/animals/GLTF/Animations/Dove_Animations.gltf"
   );
   const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
   const { nodes } = useGraph(clone);
@@ -88,7 +88,7 @@ export default function MyDoveAnimations({
       if (moving) {
         angle = Math.atan2(
           destinationPoint.z - group.current.position.z,
-          destinationPoint.x - group.current.position.x,
+          destinationPoint.x - group.current.position.x
         );
         if (isCollided) {
           group.current.position.x -= Math.cos(angle) * 0.5;
@@ -98,8 +98,8 @@ export default function MyDoveAnimations({
           setMoving(false);
           setCollision(false);
         } else {
-          group.current.position.x += Math.cos(angle) * 0.065;
-          group.current.position.z += Math.sin(angle) * 0.065;
+          group.current.position.x += Math.cos(angle) * 0.2;
+          group.current.position.z += Math.sin(angle) * 0.2;
         }
         api.position.set(group.current.position.x, 0, group.current.position.z);
         state.camera.position.x = 1 + group.current.position.x;
@@ -112,8 +112,8 @@ export default function MyDoveAnimations({
         // console.log(group.current.position);
         // console.log(Math.cos(angle) * 0.065);
         if (
-          Math.abs(destinationPoint.x - group.current.position.x) < 0.03 &&
-          Math.abs(destinationPoint.z - group.current.position.z) < 0.03
+          Math.abs(destinationPoint.x - group.current.position.x) < 0.1 &&
+          Math.abs(destinationPoint.z - group.current.position.z) < 0.1
         ) {
           setMoving(false);
           actions["Walk"].stop();
