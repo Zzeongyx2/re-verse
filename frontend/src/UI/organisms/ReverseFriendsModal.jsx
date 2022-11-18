15eccf41b870e22d3b40dfefa7ab4ec0e08a8b4e
지용현 <yung5487@gmail.com>
2022-11-19 00:00:55 +0900

    [FE] fix: change share member only my archive
    Former-commit-id: f8f9ced613ac26a83cba91f021c2831c4d6afbdb

--- frontend/src/UI/organisms/ReverseFriendsModal.jsx
+++ frontend/src/UI/organisms/ReverseFriendsModal.jsx
@@ -53,6 +53,7 @@ function ReverseFriendModal({ joinMembers }) {
   const reverse = useSelector((state) => state.reverse);
   const friendList = useSelector((state) => state.friend.friendList);
   const joinArchive = useSelector((state) => state.archive.joinArchive);
+  const loginUser = useSelector((state) => state.user.loginUser);
   const dispatch = useDispatch();
   const [findNickName, setFindNickName] = useState("");
   const [archive, setArchive] = useState();
@@ -65,7 +66,7 @@ function ReverseFriendModal({ joinMembers }) {
       getArchiveDetail(
         reverse.info.archiveId,
         getArchiveDetailSuccess,
-        getArchiveDetailFail
+        getArchiveDetailFail,
       );
     }
   }, [reverse.info.archiveId]);
@@ -93,7 +94,7 @@ function ReverseFriendModal({ joinMembers }) {
       setInfo({
         ...reverse.info,
         stuffs: res.data.stuffs,
-      })
+      }),
     );
   };
 
@@ -107,7 +108,7 @@ function ReverseFriendModal({ joinMembers }) {
       archive.id,
       nickname,
       editMemberSuccess,
-      editMemberFail
+      editMemberFail,
     );
   };
   const deleteMember = async (nickname) => {
@@ -115,7 +116,7 @@ function ReverseFriendModal({ joinMembers }) {
       archive.id,
       nickname,
       editMemberSuccess,
-      editMemberFail
+      editMemberFail,
     );
   };
   const editMemberSuccess = async (res) => {
@@ -124,7 +125,7 @@ function ReverseFriendModal({ joinMembers }) {
     await getArchiveDetail(
       reverse.info.archiveId,
       getArchiveDetailSuccess,
-      getArchiveDetailFail
+      getArchiveDetailFail,
     );
   };
   const editMemberFail = (error) => {
@@ -206,133 +207,136 @@ function ReverseFriendModal({ joinMembers }) {
                       </Popover>
                     );
                   })}
-                {/* 후추 잠시 */}
               </div>
 
               {/* 아카이브 공유 관리 */}
-              <div className="p-2 mb-4 border-2 border-basic3 rounded-lg w-full h-[calc(99%/3*2)] ">
-                <p className="font-bold px-1 pt-0.5 pb-1.5 mb-1.5">
-                  아카이브 공유 관리
-                </p>
-                {/* search bar */}
-                <div className="w-full flex justify-center items-center px-4 py-1.5 mb-5 border-2 border-base1/20 rounded-lg">
-                  <BsSearch size={24} />
-                  <input
-                    onChange={findNickNameHandleChange}
-                    value={findNickName}
-                    type="text"
-                    placeholder="닉네임을 검색하여 친구를 찾아보세요"
-                    className="w-full focus:outline-none pl-3.5 text-sm"
-                  />
-                </div>
-                {/* friendlist */}
-                <div className="flex justify-between">
-                  {/* 공유되지 않은 친구 */}
-                  <div className="w-[calc(90%/2)]">
-                    <p className="bg-basic2 w-full py-1 px-2 mb-1 rounded-md">
-                      공유되지 않은 친구
-                    </p>
-                    <div className="h-28 overflow-auto scrollbar-hide">
-                      {friendList
-                        .filter((info) => {
-                          for (
-                            let index = 0;
-                            index < archive?.members.length;
-                            index++
-                          ) {
-                            const element = archive.members[index];
-                            if (info.nickname === element.nickname) {
-                              return;
-                            }
-                          }
-                          if (findNickName.trim() === "") {
-                            return info;
-                          } else if (info.nickname.includes(findNickName)) {
-                            return info;
-                          }
-                        })
-                        .map((info, idx) => {
-                          return (
-                            <div key={`isShared-${idx}`}>
-                              <div className="flex items-center justify-between px-2 py-1">
-                                <Avatar
-                                  size="sm"
-                                  name={info.nickname}
-                                  src={s3Path + info.avatar + imageForm}
-                                />
-                                <div className="font-bold overflow-hidden text-ellipsis line-clamp-1">
-                                  {info.nickname}
-                                </div>
-                                <button
-                                  className=""
-                                  onClick={() => {
-                                    shareMember(info.nickname);
-                                  }}
-                                >
-                                  <BsFillPlusCircleFill className="text-[#7186B2] text-lg" />
-                                </button>
-                              </div>
-                            </div>
-                          );
-                        })}
-                    </div>
+              {joinArchive.members[0].nickname === loginUser.nickname && (
+                <div className="p-2 mb-4 border-2 border-basic3 rounded-lg w-full h-[calc(99%/3*2)] ">
+                  <p className="font-bold px-1 pt-0.5 pb-1.5 mb-1.5">
+                    아카이브 공유 관리
+                  </p>
+                  {/* search bar */}
+                  <div className="w-full flex justify-center items-center px-4 py-1.5 mb-5 border-2 border-base1/20 rounded-lg">
+                    <BsSearch size={24} />
+                    <input
+                      onChange={findNickNameHandleChange}
+                      value={findNickName}
+                      type="text"
+                      placeholder="닉네임을 검색하여 친구를 찾아보세요"
+                      className="w-full focus:outline-none pl-3.5 text-sm"
+                    />
                   </div>
-
-                  <BsArrowLeftRight size={22} className="mt-1" />
-
-                  {/* 공유된 친구 */}
-                  <div className="w-[calc(90%/2)]">
-                    <p className="bg-basic2 w-full py-1 px-2 mb-1 rounded-md">
-                      공유된 친구
-                    </p>
-                    <div className="h-28 overflow-auto scrollbar-hide">
-                      {friendList
-                        .filter((info) => {
-                          for (
-                            let index = 0;
-                            index < archive?.members.length;
-                            index++
-                          ) {
-                            const element = archive.members[index];
-                            if (info.nickname === element.nickname) {
-                              if (findNickName.trim() === "") {
-                                return info;
-                              } else if (info.nickname.includes(findNickName)) {
-                                return info;
+                  {/* friendlist */}
+                  <div className="flex justify-between">
+                    {/* 공유되지 않은 친구 */}
+                    <div className="w-[calc(90%/2)]">
+                      <p className="bg-basic2 w-full py-1 px-2 mb-1 rounded-md">
+                        공유되지 않은 친구
+                      </p>
+                      <div className="h-28 overflow-auto scrollbar-hide">
+                        {friendList
+                          .filter((info) => {
+                            for (
+                              let index = 0;
+                              index < archive?.members.length;
+                              index++
+                            ) {
+                              const element = archive.members[index];
+                              if (info.nickname === element.nickname) {
+                                return;
                               }
                             }
-                          }
-                        })
-                        .map((info, idx) => {
-                          return (
-                            <div key={`isNotShared-${idx}`}>
-                              <div className="flex items-center justify-between px-2 py-1">
-                                <Avatar
-                                  size="sm"
-                                  name={info.nickname}
-                                  src={s3Path + info.avatar + imageForm}
-                                />
-                                <div className="font-bold overflow-hidden text-ellipsis line-clamp-1">
-                                  {info.nickname}
+                            if (findNickName.trim() === "") {
+                              return info;
+                            } else if (info.nickname.includes(findNickName)) {
+                              return info;
+                            }
+                          })
+                          .map((info, idx) => {
+                            return (
+                              <div key={`isShared-${idx}`}>
+                                <div className="flex items-center justify-between px-2 py-1">
+                                  <Avatar
+                                    size="sm"
+                                    name={info.nickname}
+                                    src={s3Path + info.avatar + imageForm}
+                                  />
+                                  <div className="font-bold overflow-hidden text-ellipsis line-clamp-1">
+                                    {info.nickname}
+                                  </div>
+                                  <button
+                                    className=""
+                                    onClick={() => {
+                                      shareMember(info.nickname);
+                                    }}
+                                  >
+                                    <BsFillPlusCircleFill className="text-[#7186B2] text-lg" />
+                                  </button>
                                 </div>
-                                <button
-                                  onClick={() => {
-                                    deleteMember(info.nickname);
-                                  }}
-                                >
-                                  <BsFillDashCircleFill className="text-[#F8C67E] text-lg" />
-                                </button>
                               </div>
-                            </div>
-                          );
-                        })}
+                            );
+                          })}
+                      </div>
+                    </div>
+
+                    <BsArrowLeftRight size={22} className="mt-1" />
+
+                    {/* 공유된 친구 */}
+                    <div className="w-[calc(90%/2)]">
+                      <p className="bg-basic2 w-full py-1 px-2 mb-1 rounded-md">
+                        공유된 친구
+                      </p>
+                      <div className="h-28 overflow-auto scrollbar-hide">
+                        {friendList
+                          .filter((info) => {
+                            for (
+                              let index = 0;
+                              index < archive?.members.length;
+                              index++
+                            ) {
+                              const element = archive.members[index];
+                              if (info.nickname === element.nickname) {
+                                if (findNickName.trim() === "") {
+                                  return info;
+                                } else if (
+                                  info.nickname.includes(findNickName)
+                                ) {
+                                  return info;
+                                }
+                              }
+                            }
+                          })
+                          .map((info, idx) => {
+                            return (
+                              <div key={`isNotShared-${idx}`}>
+                                <div className="flex items-center justify-between px-2 py-1">
+                                  <Avatar
+                                    size="sm"
+                                    name={info.nickname}
+                                    src={s3Path + info.avatar + imageForm}
+                                  />
+                                  <div className="font-bold overflow-hidden text-ellipsis line-clamp-1">
+                                    {info.nickname}
+                                  </div>
+                                  <button
+                                    onClick={() => {
+                                      deleteMember(info.nickname);
+                                    }}
+                                  >
+                                    <BsFillDashCircleFill className="text-[#F8C67E] text-lg" />
+                                  </button>
+                                </div>
+                              </div>
+                            );
+                          })}
+                      </div>
                     </div>
                   </div>
-                </div>
-                {/* {friendArr.map((info, idx) => {
+                  {/* {friendArr.map((info, idx) => {
                   return <div>{info.nickname}</div>;
                 })} */}
-              </div>
+                </div>
+              )}
             </div>
           </ModalBody>
           {/* <ModalFooter></ModalFooter> */}
