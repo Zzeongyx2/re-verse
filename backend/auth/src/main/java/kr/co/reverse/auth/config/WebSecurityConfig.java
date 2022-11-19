1a57dadfb2991c893237da7037d976604abcf357
박원창 <parkwc0213@naver.com>
2022-11-19 17:03:16 +0900

    [BE] fix: elastic setting test
    Former-commit-id: 5e6bf6c11f021c9f41151c06faaeb9fbb381c8b9

--- backend/auth/src/main/java/kr/co/reverse/auth/config/WebSecurityConfig.java
+++ backend/auth/src/main/java/kr/co/reverse/auth/config/WebSecurityConfig.java
@@ -83,6 +83,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.addAllowedOrigin("https://re-verse.co.kr");
         configuration.addAllowedOrigin("https://re-verse.co.kr:3000");
         configuration.addAllowedOrigin("https://re-verse.kr");
+        configuration.addAllowedOrigin("https://re-verse.kr:80");
+        configuration.addAllowedOrigin("https://re-verse.kr:3000");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.addExposedHeader("Authorization");
