bb4108047fd6da688758cb6389dbdb6cb65d3db2
박원창 <parkwc0213@naver.com>
2022-11-19 17:03:16 +0900

    [BE] fix: elastic setting test
    Former-commit-id: 4c58b9ef59baff35daa7ba208d0d540fcab20d11

---
+++ backend/archive/src/main/java/kr/co/reverse/archive/common/config/AwsCredentialsConfig.java
@@ -0,0 +1,23 @@
+//package kr.co.reverse.archive.common.config;
+//
+//import com.amazonaws.auth.AWSCredentials;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import com.amazonaws.auth.*;
+//@Configuration
+//public class AwsCredentialsConfig {
+//
+//    @Value("${elasticsearch.iam}")
+//    private String iam;
+//
+//    @Value("${elasticsearch.endpoint")
+//    private String endPoint;
+//
+//    @Value("${elasticsearch.region")
+//    private String region;
+//
+//    @Bean
+//    public AwsCredentialsConfig permissionProvider(){
+//    }
+//}
