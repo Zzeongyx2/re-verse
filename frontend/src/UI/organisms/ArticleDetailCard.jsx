3c214c31a4bf840651e1763e6b0f058580b3dd6a
yoonsunny17 <jyoonsun0217@gmail.com>
2022-11-20 17:02:27 +0900

    [FE] fix: modal cell phone bg
    Former-commit-id: d0e701d68d88631b4d3c40e2ec43fa63b6a8cde0

--- frontend/src/UI/organisms/ArticleDetailCard.jsx
+++ frontend/src/UI/organisms/ArticleDetailCard.jsx
@@ -1,4 +1,10 @@
-import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CloseButton,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deletePaper, getStuffDetail } from "../../api/reverse";
@@ -11,6 +17,10 @@ import {
   setTravel,
 } from "../../modules/reverse";
 import { BiArrowBack } from "react-icons/bi";
+import { TbBluetoothConnected } from "react-icons/tb";
+import { HiWifi } from "react-icons/hi";
+import { MdOutlineSignalCellularAlt } from "react-icons/md";
+import { RiBattery2ChargeFill } from "react-icons/ri";
 
 function ArticleDetailCard() {
   const dispatch = useDispatch();
@@ -60,10 +70,97 @@ function ArticleDetailCard() {
   const deleteFail = (err) => {
     console.log(err);
   };
+  const [time, setTime] = useState("00:00");
+  const timeSet = new Set();
+
+  const clock = () => {
+    const time = new Date();
+    const hours = time.getHours() % 24;
+    const minutes = time.getMinutes();
+
+    setTime(
+      `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      }`
+    );
+  };
+
+  useEffect(() => {
+    setInterval(clock, 1000);
+  }, []);
+
   return (
     info.details && (
-      <div className="font-hand">
-        <Card width={"xs"}>
+      <div className="mt-2 w-[344px] h-[600px] bg-white flex flex-col py-2 px-4 rounded-lg ring-8 border-4 ring-gray-200 border-gray-200">
+        {/* Status Bar */}
+        <div className="w-100 flex justify-between mb-2">
+          <div>
+            <span className="font-semibold text-gray-700 mr-2">SSAFY</span>
+            <span>{time}</span>
+          </div>
+          <div className="flex">
+            <TbBluetoothConnected className="text-gray-700 text-2xl" />
+            <HiWifi className="ml-1 text-gray-700 text-2xl" />
+            <MdOutlineSignalCellularAlt className="text-gray-700 text-2xl" />
+            <RiBattery2ChargeFill className="text-gray-700 text-2xl" />
+          </div>
+        </div>
+
+        {/* App Nav Bar */}
+        <div className="flex justify-between font-neon">
+          <div className="text-xl font-semibold">RE-VERSE</div>
+          <button onClick={() => dispatch(setIsCardOpen())}>
+            <BiArrowBack size={20} />
+          </button>
+        </div>
+        <hr className="mt-1"></hr>
+
+        {/* Article detail */}
+        <div className="font-hand mt-1">
+          {/* memory date */}
+          <div className="text-sm text-basic1 text-end">
+            기록날짜 :{" "}
+            {new Date(info.details.memoryTime).toLocaleDateString() + " "}
+            {weekDay[new Date(info.details.memoryTime).getDay()]}
+          </div>
+          {/* title */}
+          <div className="text-lg font-semibold">{info.details.title}</div>
+          {/* createdTime */}
+          {/* <div className="text-sm text-basic1">
+            생성날짜 : {info.details.createdTime.slice(0, -9)}
+          </div> */}
+          {/* lastEditedTime */}
+          {/* <div className="text-sm text-basic1">
+            최근 수정날짜 : {info.details.lastEditedTime.slice(0, -9)}
+          </div> */}
+          {/* content */}
+          <div className="mx-2 mt-2 h-[410px] overflow-auto scrollbar-hide">
+            <div
+              dangerouslySetInnerHTML={{ __html: info.details.content }}
+            ></div>
+          </div>
+
+          {/* edit & delete */}
+          <hr className=""></hr>
+          <div className="h-12 flex justify-between items-center px-10">
+            <button
+              onClick={() => {
+                dispatch(setEditBtn());
+              }}
+            >
+              수정하기
+            </button>
+            <button
+              onClick={() => {
+                handleDelete();
+                dispatch(setIsCardOpen());
+              }}
+            >
+              삭제하기
+            </button>
+          </div>
+        </div>
+        {/* <Card width={"xs"}>
           <CardHeader px={4} py={0}>
             <div className="flex justify-between">
               <div className="text-lg font-semibold">{info.details.title}</div>
@@ -83,7 +180,7 @@ function ArticleDetailCard() {
               </div>
             )}
           </CardHeader>
-          <CardBody minH={400}>
+          <CardBody minH={323}>
             <div
               dangerouslySetInnerHTML={{ __html: info.details.content }}
             ></div>
@@ -114,7 +211,7 @@ function ArticleDetailCard() {
               삭제하기
             </button>
           </CardFooter>
-        </Card>
+        </Card> */}
       </div>
     )
   );
