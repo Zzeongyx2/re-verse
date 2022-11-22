756d7b68fb6d6a8690f43c29221120df3cc84175
yoonsunny17 <jyoonsun0217@gmail.com>
2022-11-22 23:19:29 +0900

    [FE] fix: delete useless codes
    Former-commit-id: 962ee1d131153703b258061910d1ca9dad618447 [formerly c494fcccadca8d62b67d4f3572d47c64864cbb2e] [formerly 8e25a7229d800abb937124a8b04b181a7943caaa [formerly dd6cd8aa3f5e95646ecc80c938d70f55025a0ad9]]
    Former-commit-id: 8e25a7229d800abb937124a8b04b181a7943caaa
    Former-commit-id: 28afb8f85a425099ff36d2389ab2a4929b3be9ea

--- frontend/src/theme/components/avatar.js
+++ frontend/src/theme/components/avatar.js
@@ -1,5 +1,5 @@
 import { avatarAnatomy } from "@chakra-ui/anatomy";
-import { createMultiStyleConfigHelpers, defineStyle } from "@chakra-ui/react";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
 
 const { definePartsStyle, defineMultiStyleConfig } =
   createMultiStyleConfigHelpers(avatarAnatomy.keys);
