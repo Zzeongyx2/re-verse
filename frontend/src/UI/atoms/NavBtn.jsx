07155752e7500ba882c05469fe117bb1e9dba369
yoonsun <jyoonsun0217@gmail.com>
2022-11-25 00:17:25 +0900

    [FE] feat: main lobby logout btn
    Former-commit-id: 957ccbbe2d6cba655ef8e7bbfec498346b0a52db [formerly c6d82b8440a1cfe15906c1667aebc3f0c6b5e90d] [formerly 23f625b0e23438023873008610b77b798d0232a5 [formerly cf81faf150770297a57f99d619ae40509159cc27]]
    Former-commit-id: 23f625b0e23438023873008610b77b798d0232a5
    Former-commit-id: 71b0bdb443663328d2c48803b2e4250221c7d9ca

--- frontend/src/UI/atoms/NavBtn.jsx
+++ frontend/src/UI/atoms/NavBtn.jsx
@@ -1,9 +1,11 @@
-function NavBtn({ icon, to, from }) {
+function NavBtn({ icon, to, from, onClick }) {
   return (
     <div
       className={`border border-1 border-white rounded-xl p-2 bg-gradient-to-t ${from} ${to} hover:from-main2 hover:to-sub2`}
     >
-      <button className="flex justify-center">{icon}</button>
+      <button onClick={onClick} className="flex justify-center">
+        {icon}
+      </button>
     </div>
   );
 }
